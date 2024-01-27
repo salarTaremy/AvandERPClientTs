6f274de55af67258d617f4031a9488b9832d7141
zand23 <52084239+zand23@users.noreply.github.com>
2024-01-27 16:20:15 +0330

    fix logo sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,38 +1,38 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import {
   AppstoreOutlined,
   SettingOutlined,
   BellOutlined,
   FileOutlined,
   PieChartOutlined,
-} from '@ant-design/icons'
-import logo from './../assets/images/logos/Logo2.png'
-import PropTypes from 'prop-types'
-import { Layout, Menu, Image } from 'antd'
+} from "@ant-design/icons";
+import logo from "./../assets/images/logos/Logo2.png";
+import logoFlat from "./../assets/images/logos/LogoIcon128_Flat.png";
+import logoShadow from "./../assets/images/logos/LogoIcon128_Shadow.png";
+import PropTypes from "prop-types";
+import { Layout, Menu, Image } from "antd";
 
-const { Sider } = Layout
+const { Sider } = Layout;
 
 const slider = {
-  overflowX: 'auto',
-  height: '100vh',
+  overflowX: "auto",
+  height: "100vh",
   right: 0,
   top: 0,
   bottom: 0,
-}
-
+};
 
 //====================================================================
 //                        Component
 //====================================================================
 const AppSidebar = (props) => {
-  const { showImageSider,collapsedSider,items} = props
+  const { showImageSider, collapsedSider, items } = props;
   // const [collapsed, setCollapsed] = useState(false)
-
 
   return (
     <>
       <Sider
-          width={280}
+        width={280}
         className="sidebar desktop-only"
         style={slider}
         collapsed={collapsedSider}
@@ -41,22 +41,40 @@ const AppSidebar = (props) => {
         {!showImageSider && (
           <Image
             preview={false}
-            style={{ margin: '20px 50px 20px 20px', textAlign: 'center' }}
+            style={{ margin: "20px 50px 20px 20px", textAlign: "center" }}
             width={200}
             src={logo}
           />
         )}
+        <Image
+          className={!showImageSider ? "hidden" : ""}
+          preview={false}
+          style={{ margin: "5px 10px ", textAlign: "center" }}
+          width={60}
+          src={logoFlat}
+        />
+        {/* <Image
+          className={!showImageSider ? "visibility" : ""}
+          preview={false}
+          style={{ textAlign: "center" }}
+          width={80}
+          src={logoShadow}
+        /> */}
+
         <div className="demo-logo-vertical" />
-        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
+        <Menu
+          theme="dark"
+          defaultSelectedKeys={["1"]}
+          mode="inline"
+          items={items}
+        />
       </Sider>
     </>
-  )
-}
-export default AppSidebar
+  );
+};
+export default AppSidebar;
 AppSidebar.propTypes = {
   showImageSider: PropTypes.bool,
   collapsedSider: PropTypes.bool,
   items: PropTypes.object,
-
-
-}
+};
