585674882f6bf502abbac520122e6dd8e1fb61ba
salar_taremy <salar_taremy@yahoo.com>
2024-02-06 17:15:25 +0330

    ok

--- src/Pages/accounting/accountDocument/list/AccountDocumentDetailView.jsx
+++ src/Pages/accounting/accountDocument/list/AccountDocumentDetailView.jsx
@@ -6,66 +6,64 @@ import PropTypes from "prop-types";
 import Loading from '@/components/common/Loading';
 const AccountDocumentDetailView = (props) => {
 
-const cl = [
-  {
-    title: 'کد',
-    dataIndex: 'code',
-    key: 'code',    
-  },
-  {
-    title: 'نام',
-    dataIndex: 'name',
-    key: 'name',    
-  },
-  {
-    title: 'ردیف',
-    dataIndex: 'rowNumber',
-    key: 'rowNumber',
-    align: 'center',
-  },
-  {
-    title: 'شرح آرتیکل',
-    dataIndex: 'article',
-    key: 'article',    
-  },
-  {
-    title: 'بدهکار',
-    dataIndex: 'debtor',
-    key: 'debtor',    
-  },
-  {
-    title: 'بستانکار',
-    dataIndex: 'referenceNo',
-    key: 'referenceNo',    
-  },
-  {
-    title: 'توضیحات',
-    dataIndex: 'description',
-    key: 'description',    
-  },
-]
+  const cl = [
+    {
+      title: 'کد',
+      dataIndex: 'code',
+      key: 'code',
+    },
+    {
+      title: 'نام',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'ردیف',
+      dataIndex: 'rowNumber',
+      key: 'rowNumber',
+      align: 'center',
+    },
+    {
+      title: 'شرح آرتیکل',
+      dataIndex: 'article',
+      key: 'article',
+    },
+    {
+      title: 'بدهکار',
+      dataIndex: 'debtor',
+      key: 'debtor',
+    },
+    {
+      title: 'بستانکار',
+      dataIndex: 'referenceNo',
+      key: 'referenceNo',
+    },
+    {
+      title: 'توضیحات',
+      dataIndex: 'description',
+      key: 'description',
+    },
+  ]
 
 
-    const { id } = props
-    const [data, loading, error] = api.useFetch(url.ACCOUNT_DOCUMENT_DETAIL + '?AccountingDocumentID=' + id.toString())
-    return (
-      <>
-       {loading &&  <Loading    message= "لطفا کمی صبر کنید" description = {`درحال دانلود اطلاعات آرتیکل سند شناسه ${id}`} />
-       ||
-<>
-      <Ant.Table   columns={cl} dataSource={data?.data || null}/>
-
-
-       <Ant.Typography >
-        <pre>{JSON.stringify(data?.data, null, 2)}</pre>
-      </Ant.Typography>
-      </>
+  const { id } = props
+  const [data, loading, error] = api.useFetch(url.ACCOUNT_DOCUMENT_DETAIL + '?AccountingDocumentID=' + id.toString())
+  return (
+    <>
+      {loading && <Loading message="لطفا کمی صبر کنید" description={`درحال دانلود اطلاعات آرتیکل سند شناسه ${id}`} />
+        ||
+        <>
+          <Ant.Table columns={cl} dataSource={data?.data || null} />
+          <Ant.Typography >
+            <pre>{JSON.stringify(data?.data, null, 2)}</pre>
+          </Ant.Typography>
+        </>
+      }
+    </>
+  )
 }
-       </>
-    )
-  }
-  AccountDocumentDetailView.propTypes = {
-    id: PropTypes.any,
-  }
+AccountDocumentDetailView.propTypes = {
+  id: PropTypes.any,
+}
 
-  export default AccountDocumentDetailView
+export default AccountDocumentDetailView
