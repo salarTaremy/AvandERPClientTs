a293f9b75de48aae2b302e9faba30ca118c80699
Salar <salar_taremy@yahoo.com>
2024-02-09 23:27:42 +0330

    ok

--- src/Pages/accounting/reports/hybridBrowsing/HybridBrowsing.jsx
+++ src/Pages/accounting/reports/hybridBrowsing/HybridBrowsing.jsx
@@ -1,52 +1,56 @@
-import React,{useEffect,useState} from 'react'
-import { PropTypes } from 'prop-types'
-import * as Ant from 'antd'
-import * as url from '@/api/url'
-import * as styles from '@/styles'
-import * as defaultValues from '@/defaultValues'
-import * as api from '@/api'
-import useRequestManager from '@/hooks/useRequestManager'
-import useAllLoading from '@/hooks/useAllLoading '
-import { columns } from './columns'
-import ButtonList from '@/components/common/ButtonList'
-import FilterPanel from './FilterPanel'
-import FilterBedge from '@/components/common/FilterBedge'
-import FilterDrawer from '@/components/common/FilterDrawer'
+import React, { useEffect, useState } from "react";
+import { PropTypes } from "prop-types";
+import * as Ant from "antd";
+import * as url from "@/api/url";
+import * as styles from "@/styles";
+import * as defaultValues from "@/defaultValues";
+import * as api from "@/api";
+import useRequestManager from "@/hooks/useRequestManager";
+import useAllLoading from "@/hooks/useAllLoading ";
+import { columns } from "./columns";
+import ButtonList from "@/components/common/ButtonList";
+import FilterPanel from "./FilterPanel";
+import FilterBedge from "@/components/common/FilterBedge";
+import FilterDrawer from "@/components/common/FilterDrawer";
+import * as IconBs from "react-icons/bs";
 //====================================================================
 //                        Declaration
 //====================================================================
 const HybridBrowsing = (props) => {
-  const {id} = props
-  const pageTitle = 'مرور ترکیبی حسابها'
-  const [listData, listLoading, listError, listApiCall] = api.useFetchWithHandler()
-  const [openFilter, setOpenFilter] = useState(false)
-  const [filterObject, setFilterObject] = useState()
-  const [filterCount, setFilterCount] = useState(0)
-  useRequestManager({ error: listError })
+  const { id } = props;
+  const pageTitle = "مرور ترکیبی حسابها";
+  const [listData, listLoading, listError, listApiCall] =
+    api.useFetchWithHandler();
+  const [openFilter, setOpenFilter] = useState(false);
+  const [filterObject, setFilterObject] = useState();
+  const [filterCount, setFilterCount] = useState(0);
+  useRequestManager({ error: listError });
   //====================================================================
   //                        useEffects
   //====================================================================
   useEffect(() => {
     filterObject &&
-      setFilterCount(Object.keys(filterObject)?.filter((key) => filterObject[key])?.length)
-    !filterObject && setFilterCount(0)
-    fillGrid()
+      setFilterCount(
+        Object.keys(filterObject)?.filter((key) => filterObject[key])?.length,
+      );
+    !filterObject && setFilterCount(0);
+    fillGrid();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [filterObject])
+  }, [filterObject]);
   //====================================================================
   //                        Functions
   //====================================================================
   const fillGrid = async () => {
-    await listApiCall(url.DETAILED_ACCOUNT)
-  }
+    await listApiCall(url.DETAILED_ACCOUNT);
+  };
   const onFilterChanged = async (filterObject) => {
-    setFilterObject(filterObject)
-    setOpenFilter(false)
-  }
+    setFilterObject(filterObject);
+    setOpenFilter(false);
+  };
   const onRemoveFilter = () => {
-    setFilterObject(null)
-    setOpenFilter(false)
-  }
+    setFilterObject(null);
+    setOpenFilter(false);
+  };
   //====================================================================
   //                        Child Components
   //====================================================================
@@ -58,14 +62,14 @@ const HybridBrowsing = (props) => {
       <ButtonList
         filterCount={filterCount}
         onAdd={() => {
-          alert('Add Click')
+          alert("Add Click");
         }}
         onFilter={() => {
-          setOpenFilter(true)
+          setOpenFilter(true);
         }}
       />
-    )
-  }
+    );
+  };
   const Grid = () => {
     return (
       <>
@@ -76,8 +80,8 @@ const HybridBrowsing = (props) => {
           dataSource={(listData?.isSuccess && listData?.data) || null}
         />
       </>
-    )
-  }
+    );
+  };
   //====================================================================
   //                        Component
   //====================================================================
@@ -85,17 +89,59 @@ const HybridBrowsing = (props) => {
     <Ant.Card Card title={pageTitle} type="inner">
       <Ant.Card style={{ ...styles.CARD_DEFAULT_STYLES }} loading={false}>
       <FilterDrawer
-          open={openFilter}
-          onClose={() => setOpenFilter(false)}
-          onRemoveFilter={onRemoveFilter}
-        >
-          <FilterPanel filterObject={filterObject} onSubmit={onFilterChanged} />
-        </FilterDrawer>
-        <FilterBedge filterCount={filterCount}>
-          <Grid />
-        </FilterBedge>
+              open={openFilter}
+              onClose={() => setOpenFilter(false)}
+              onRemoveFilter={onRemoveFilter}
+            >
+              <FilterPanel
+                filterObject={filterObject}
+                onSubmit={onFilterChanged}
+              />
+            </FilterDrawer>
+        <Ant.Row>
+          <Ant.Col span={24}>
+            <Ant.Space align="center">
+              <Ant.Button type="text">
+                <IconBs.BsAlignStart />
+              </Ant.Button>
+              <Ant.Button type="text">
+                <IconBs.Bs0Square />
+              </Ant.Button>
+              <Ant.Button type="text">
+                <IconBs.BsFillCreditCard2FrontFill />
+              </Ant.Button>
+              <Ant.Button type="text">
+                <IconBs.BsJournalMinus />
+              </Ant.Button>
+            </Ant.Space>
+          </Ant.Col>
+        </Ant.Row>
+        <Ant.Row>
+          <Ant.Col span={1}>
+            <Ant.Space  direction="vertical"  align="center">
+              <Ant.Button type="text">
+                <IconBs.BsAlignStart />
+              </Ant.Button>
+              <Ant.Button type="text">
+                <IconBs.Bs0Square />
+              </Ant.Button>
+              <Ant.Button type="text">
+                <IconBs.BsFillCreditCard2FrontFill />
+              </Ant.Button>
+              <Ant.Button type="text">
+                <IconBs.BsJournalMinus />
+              </Ant.Button>
+            </Ant.Space>
+          </Ant.Col>
+          <Ant.Col span={23}>
+          
+            <FilterBedge filterCount={filterCount}>
+              <Grid />
+            </FilterBedge>
+          </Ant.Col>
+        </Ant.Row>
       </Ant.Card>
     </Ant.Card>
-  )
-}
-export default HybridBrowsing
+  );
+};
+export default HybridBrowsing;
