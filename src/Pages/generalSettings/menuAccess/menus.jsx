24e9579d28e487899836731ef8c587f2a7fa1857
zand23 <52084239+zand23@users.noreply.github.com>
2024-02-26 16:20:44 +0330

    fix Access Menus

--- src/Pages/generalSettings/menuAccess/menus.jsx
+++ src/Pages/generalSettings/menuAccess/menus.jsx
@@ -7,13 +7,13 @@ import qs from "qs";
 import { useFetchWithHandler, useFetch } from "@/api";
 import useRequestManager from "@/hooks/useRequestManager";
 
-
 const Menus = () => {
   const [expandedKeys, setExpandedKeys] = useState(["0-0-0", "0-0-1"]);
   const [checkedKeys, setCheckedKeys] = useState(["0-0-0"]);
   const [selectedKeys, setSelectedKeys] = useState([]);
   const [idRol, setIdRol] = useState(0);
   const [disable, setDisable] = useState(true);
+  const [disableButton, setDisableButton] = useState(true);
   const [items, setItems] = useState([]);
   const [autoExpandParent, setAutoExpandParent] = useState(true);
   const [data, loading, error, apiCall] = useFetchWithHandler();
@@ -42,13 +42,17 @@ const Menus = () => {
   const onCheck = (checkedKeysValue) => {
     console.log("onCheck", checkedKeysValue);
     setCheckedKeys(checkedKeysValue);
+    setDisableButton(false);
   };
   const onSelect = (selectedKeysValue, info) => {
     console.log("onSelect", info);
     setSelectedKeys(selectedKeysValue);
   };
+  const onsubmit = () => {
+    // setDisableButton(false);
+  };
   const onChangeRoleScope = async (val) => {
-console.log(val,"val")
+    console.log(val, "val");
     const data = qs.stringify({
       Id: parseInt(val),
     });
@@ -57,65 +61,77 @@ console.log(val,"val")
   };
   return (
     <Ant.Card Card title={"دسترسی منوها"} type="inner">
-      <Ant.Row gutter={[16, 8]}>
-        <Ant.Col span={24} sm={12}>
-          <Ant.Card style={{ ...styles.CARD_DEFAULT_STYLES }}>
+      <Ant.Row  gutter={[16, 16]}>
+        <Ant.Col lg={12} md={12} sm={24} xs={24}>
+          <Ant.Card
+            className="w-full"
+            style={{ ...styles.CARD_DEFAULT_STYLES }}
+          >
             <Ant.Form form={form} layout="vertical" onFinish={null}>
-              <Ant.Row gutter={[16, 8]}>
-                <Ant.Col span={24} md={24} lg={24}>
-                  <Ant.Form.Item
-                    name={"accountTypeId"}
-                    label="محدود نقش"
-                    rules={[{ required: true }]}
-                  >
-                    <Ant.Select
-                      onChange={(value) => onChangeRoleScope(value)}
-                      placeholder={"انتخاب کنید..."}
-                      disabled={roleScopeLoading || false}
-                      loading={roleScopeLoading}
-                      options={roleScopeData?.data}
-                      fieldNames={{ label: "name", value: "id" }}
-                    />
-                  </Ant.Form.Item>
-                </Ant.Col>
-                <Ant.Col span={24} md={24} lg={24}>
-                  <Ant.Form.Item
-                    name={"role"}
-                    label="نقش"
-                    rules={[{ required: true }]}
-                  >
-                    <Ant.Select
-                      placeholder={"انتخاب کنید..."}
-                      onChange={(value) => setIdRol(value)}
-                      disabled={disable}
-                      loading={roleLoading}
-                      options={roleData?.data}
-                      fieldNames={{ label: "name", value: "roleScopeId" }}
-                    />
-                  </Ant.Form.Item>
-                </Ant.Col>
-                <Ant.Col className="text-center " span={24} md={24} lg={24}>
-                  <Ant.Button block  key="submit" type="primary">
-                    تایید
-                  </Ant.Button>
-                </Ant.Col>
+            <Ant.Row gutter={[8, 8]} >
+              <Ant.Col  md={12} lg={12} sm={24} xs={24}>
+                <Ant.Form.Item
+                  name={"accountTypeId"}
+                  label="محدود نقش"
+                  rules={[{ required: true }]}
+                >
+                  <Ant.Select
+                    onChange={(value) => onChangeRoleScope(value)}
+                    placeholder={"انتخاب کنید..."}
+                    disabled={roleScopeLoading || false}
+                    loading={roleScopeLoading}
+                    options={roleScopeData?.data}
+                    fieldNames={{ label: "name", value: "id" }}
+                  />
+                </Ant.Form.Item>
+              </Ant.Col>
+              <Ant.Col lg={12} md={12} sm={24} xs={24}>
+                <Ant.Form.Item
+                  name={"role"}
+                  label="نقش"
+                  rules={[{ required: true }]}
+                >
+                  <Ant.Select
+                    placeholder={"انتخاب کنید..."}
+                    onChange={(value) => setIdRol(value)}
+                    disabled={disable}
+                    loading={roleLoading}
+                    options={roleData?.data}
+                    fieldNames={{ label: "name", value: "roleScopeId" }}
+                  />
+                </Ant.Form.Item>
+              </Ant.Col>
               </Ant.Row>
+              <Ant.Col className="text-center" span={24} md={24} lg={24} xs={24}>
+                <Ant.Button
+                  disabled={disableButton}
+                  onClick={onsubmit}
+                  block
+                  key="submit"
+                  type="primary"
+                >
+                  تایید
+                </Ant.Button>
+              </Ant.Col>
             </Ant.Form>
           </Ant.Card>
         </Ant.Col>
-        <Ant.Col span={24} sm={12}>
+        <Ant.Col span={24} md={12} sm={24} xs={24}>
           <Ant.Card style={{ ...styles.CARD_DEFAULT_STYLES }}>
-            <Ant.Tree
-              checkable
-              onExpand={onExpand}
-              expandedKeys={expandedKeys}
-              autoExpandParent={autoExpandParent}
-              onCheck={onCheck}
-              checkedKeys={checkedKeys}
-              onSelect={onSelect}
-              selectedKeys={selectedKeys}
-              treeData={items}
-            />
+            <Ant.Skeleton loading={loading}>
+              <Ant.Tree
+                disabled={disable}
+                checkable
+                onExpand={onExpand}
+                expandedKeys={expandedKeys}
+                autoExpandParent={autoExpandParent}
+                onCheck={onCheck}
+                checkedKeys={checkedKeys}
+                onSelect={onSelect}
+                selectedKeys={selectedKeys}
+                treeData={items}
+              />
+            </Ant.Skeleton>
           </Ant.Card>
         </Ant.Col>
       </Ant.Row>
