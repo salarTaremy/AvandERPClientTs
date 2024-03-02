dd9c8b9d5a29ed4ba5ca04557174cd52c0fbeb62
mohebali92 <157206069+mohebali92@users.noreply.github.com>
2024-03-02 16:13:34 +0330

    edit product

--- src/Pages/inventory/product/list/FilterPanel.jsx
+++ src/Pages/inventory/product/list/FilterPanel.jsx
@@ -1,11 +1,20 @@
 import React, { useEffect } from 'react'
 import * as Ant from 'antd'
 import { PropTypes } from 'prop-types'
+import * as url from '@/api/url'
+import { useFetch } from '@/api'
+import useRequestManager from '@/hooks/useRequestManager'
 
 const FilterPanel = (props) => {
     const { onSubmit, filterObject } = props
     const [form] = Ant.Form.useForm()
+    const [brandData, brandLoading, brandError] = useFetch(url.BRAND)
+    useRequestManager({ error: brandError })
 
+    const commonOptions = {
+        showSearch: true,
+        filterOption: (input, option) => option.name.indexOf(input) >= 0,
+    }
     //====================================================================
     //                        useEffects
     //====================================================================
@@ -39,14 +48,23 @@ const FilterPanel = (props) => {
                 <Ant.Form.Item name={'seccondCode'} label="کد دوم">
                     <Ant.Input allowClear />
                 </Ant.Form.Item>
-                <Ant.Form.Item name={'brandId'} label="شناسه برند">
-                    <Ant.Input allowClear />
-                </Ant.Form.Item>
                 <Ant.Form.Item name={'name'} label="نام کالا">
                     <Ant.Input allowClear />
                 </Ant.Form.Item>
                 <Ant.Form.Item name={'seccondName'} label="نام دوم کالا">
                     <Ant.Input allowClear />
+                </Ant.Form.Item>
+                <Ant.Form.Item name={'brandId'} label="نام برند" valuePropName="checked">
+                    <Ant.Select
+                    {...commonOptions}
+                        mode="multiple"
+                        allowClear={true}
+                        placeholder={'انتخاب کنید...'}
+                        disabled={brandLoading || false}
+                        loading={brandLoading}
+                        options={brandData?.data}
+                        fieldNames={{ label: 'name', value: 'id' }}
+                    />
                 </Ant.Form.Item>
 
                 <Ant.Form.Item>
