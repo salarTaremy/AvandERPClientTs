d6dc6d1e1ba1791e837bbae48f2d051988529804
salar_taremy <salar_taremy@yahoo.com>
2024-03-10 15:22:55 +0330

    Config vite And Env.production

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  allowJs:true,
+  allowJs: true,
   plugins: [react()],
   server: { port: 3300 },
   resolve: {
@@ -11,4 +11,21 @@ export default defineConfig({
       '@': '/src',
     },
   },
+
+  build: {
+    chunkSizeWarningLimit: 3000,
+  },
+
+  // build: {
+  //   rollupOptions: {
+  //     output: {
+  //       manualChunks(id) {
+  //         if (id.includes('node_modules')) {
+  //           return id.toString().split('node_modules/')[1].split('/')[0].toString();
+  //         }
+  //       }
+  //     }
+  //   }
+  // }
+
 })
