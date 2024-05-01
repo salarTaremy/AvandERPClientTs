469b8b59e3b1254b214038550efe33e451615f05
salar taremi <Salar_taremy@yahoo.com>
2024-05-01 09:10:52 +0330

    Dashboard

--- src/Pages/dashboard/Dashboard.jsx
+++ src/Pages/dashboard/Dashboard.jsx
@@ -22,39 +22,17 @@ const Dashboard = () => {
 
   return (
     <>
-      <Ant.Segmented
+      {/* <Ant.Segmented
         options={PersianYears}
         block
         onChange={(val) => {
           setCurrentYear(val)
         }}
-      />
+      /> */}
       {/* {currentYear === 1402 && <GanttA />} */}
       {/* {currentYear === 1403 && <GanttB />} */}
 
-      <Ant.Form>
-        <Ant.Form.Item
-          label="Name"
-          name="name"
-          rules={[
-            {
-              required: true,
-              message: 'The name is required.',
-            },
-            {
-              validator: (_, value) => {
-                if (validateNationalCode(value?.toString())) {
-                  return Promise.resolve();
-                } else {
-                  return Promise.reject('کد ملی نا معتبر');
-                }
-              }
-            }
-          ]}
-        >
-          <Ant.Input style={{ width: '400px' }} />
-        </Ant.Form.Item>
-      </Ant.Form>
+
 
 
     </>
