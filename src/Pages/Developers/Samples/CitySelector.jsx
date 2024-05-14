f258b92d28bcb0c98184a874a3fcd8ddff5e4f67
Salar <salar_taremy@yahoo.com>
2024-05-14 20:31:21 +0330

    ok

--- src/Pages/Developers/Samples/CitySelector.jsx
+++ src/Pages/Developers/Samples/CitySelector.jsx
@@ -1,83 +1,97 @@
-
-import React, { useEffect, useState } from 'react'
-import { PropTypes } from 'prop-types'
-import * as Ant from 'antd'
-import * as url from '@/api/url'
-import * as styles from '@/styles'
-import * as api from '@/api'
-import useRequestManager from '@/hooks/useRequestManager'
-import useAllLoading from '@/hooks/useAllLoading '
+import React, { useEffect, useState } from "react";
+import { PropTypes } from "prop-types";
+import * as Ant from "antd";
+import * as url from "@/api/url";
+import * as styles from "@/styles";
+import * as api from "@/api";
+import useRequestManager from "@/hooks/useRequestManager";
+import useAllLoading from "@/hooks/useAllLoading ";
 //====================================================================
 //                        Declaration
 //====================================================================
 const CitySelector = (props) => {
-    const { id } = props
-    const pageTitle = 'شرح صفحه'
-    const [CityData, CityLoading, CityError, CityApiCall] = api.useFetchWithHandler()
-    const [options, setOptions] = useState([])
-      
+  const { id } = props;
+  const [form] = Ant.Form.useForm()
+  const pageTitle = "شرح صفحه";
+  const [CityData, CityLoading, CityError, CityApiCall] =
+    api.useFetchWithHandler();
+  const [options, setOptions] = useState([]);
+  const [selectedItem, setSelectedItem] = useState({ value: null });
+  useRequestManager({ error: CityError });
+  //...
+  //====================================================================
+  //                        useEffects
+  //====================================================================
 
-
-    const [selectedItem, setSelectedItem] = useState({value:null})
-    useRequestManager({ error: CityError })
-    //...
-    //====================================================================
-    //                        useEffects
-    //====================================================================
-
-    useEffect(async () => {
-        await CityApiCall(url.CITY_TREE)
-    }, [])
-    useEffect(() => {
-        CityData?.isSuccess && setOptions(CityData?.data)
-    }, [CityData])
-    //====================================================================
-    //                        Functions
-    //====================================================================
-    const onChange = (value, selectedOptions) => {
-        setSelectedItem({ value })
-    };
-    const filter = (inputValue, path) =>
-        path.some((option) => option.name.toLowerCase().indexOf(inputValue.toLowerCase()) > -1);
-    //====================================================================
-    //                        Child Components
-    //====================================================================
-
-
+  useEffect(() => {
+    CityApiCall(url.CITY_TREE);
+  }, []);
+  useEffect(() => {
+    CityData?.isSuccess && setOptions(CityData?.data);
+  }, [CityData]);
+  //====================================================================
+  //                        Functions
+  //====================================================================
+  function onChange(value, selectedOptions) {
+    setSelectedItem(value);
+    console.log("Onchenge", selectedOptions);
+  }
+  const filter = (inputValue, path) =>
+    path.some(
+      (option) =>
+        option.name.toLowerCase().indexOf(inputValue.toLowerCase()) > -1,
+    );
+  //====================================================================
+  //                        Child Components
+  //====================================================================
+  const onFinish = async (values) => {
     
-    const CitySelector = () => {
-        return (
-            <>
-
-
-                <Ant.Cascader
-                    loading={CityLoading}
-                    options={options}
-                    onChange={onChange}
-                    placeholder="لطفا انتخاب کنید ..."
-                    fieldNames={{ label: 'name', value: 'id', children: 'children' }}
-                    showSearch={{
-                        filter,
-                    }}
-                    onSearch={(value) => console.log(value)}
-                />
-                <Ant.Divider></Ant.Divider>
-                {JSON.stringify(selectedItem, null, 1, 1)}
-
-            </>
-        )
-    }
-    //====================================================================
-    //                        Component
-    //====================================================================
+    console.log("onFinish", values);
+  }
+  const CitySelector = () => {
     return (
-        <Ant.Card Card title={pageTitle} type="inner" style={{ ...styles.CARD_DEFAULT_STYLES }} loading={false}>
-            <CitySelector />
-        </Ant.Card>
-    )
-}
+      <>
+        <Ant.Form  form={form} onFinish={onFinish} >
+          <Ant.Form.Item name={'ww'}>
+            <Ant.Cascader
+              loading={CityLoading}
+              options={options}
+              onChange={onChange}
+              placeholder="لطفا انتخاب کنید ..."
+              fieldNames={{ label: "name", value: "id", children: "children" }}
+              showSearch={{
+                filter,
+              }}
+              onSearch={(value) => console.log(value)}
+            />
+          </Ant.Form.Item>
+          <Ant.Button onClick={() => {form.submit()}}>
+            ok
+          </Ant.Button>
+        </Ant.Form>
 
-export default CitySelector
+        <Ant.Divider></Ant.Divider>
+        {JSON.stringify(selectedItem, null, 1, 1)}
+      </>
+    );
+  };
+  //====================================================================
+  //                        Component
+  //====================================================================
+  return (
+    <Ant.Card
+      Card
+      title={pageTitle}
+      type="inner"
+      style={{ ...styles.CARD_DEFAULT_STYLES }}
+      loading={false}
+    >
+      <CitySelector />
+    </Ant.Card>
+  );
+};
+
+export default CitySelector;
 CitySelector.propTypes = {
-    id: PropTypes.any,
-}
+  id: PropTypes.any,
+};
