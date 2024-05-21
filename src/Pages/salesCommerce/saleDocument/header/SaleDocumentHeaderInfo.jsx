2778847b2a3b9c0ca6eb8f9069e0ff9c68b38a4f
zand23 <52084239+zand23@users.noreply.github.com>
2024-05-21 10:54:24 +0330

    add details customer to sale document

--- src/Pages/salesCommerce/saleDocument/header/SaleDocumentHeaderInfo.jsx
+++ src/Pages/salesCommerce/saleDocument/header/SaleDocumentHeaderInfo.jsx
@@ -1,64 +1,101 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import * as Ant from "antd";
 import { Typography } from "antd";
-
+import CustomerDescription from "../../../salesCommerce/basicInformation/CustomerManagement/description/CustomerDescription";
+import * as defaultValues from "@/defaultValues";
 //====================================================================
 //                        Declaration
 //====================================================================
 const SaleDocumentHeaderInfo = (props) => {
-    const { id, saleDocumentData, loading, className } = props;
-    const title = `${saleDocumentData?.saleDocumentType} شماره ${saleDocumentData?.documentNumber}`;
-    const referenceDocumentNumber =  saleDocumentData?.referenceDocumentNumber !== 0 ? <Typography.Link href="#">{`${saleDocumentData?.referenceDocumentType} شماره ${saleDocumentData?.referenceDocumentNumber}`}</Typography.Link> : '-';
-    const borderedItems = [
-        {
-            key: "1",
-            label: 'شماره سریال',
-            children: saleDocumentData?.serialNumber
-        },
-        {
-            key: "2",
-            label: "برگه مرجع",
-            children: referenceDocumentNumber
-        },
-        {
-            key: "3",
-            label: "کانال فروش",
-            children: saleDocumentData?.saleChannel
-        },
-        {
-            key: "4",
-            label: "نام شعبه",
-            children: saleDocumentData?.branchName
-        },
-        {
-            key: "6",
-            label: "مشتری",
-            children: <Typography.Link href="#">{saleDocumentData?.customerName}</Typography.Link> //customerId
-        },
-        {
-            key: "5",
-            label: "زمان صدور",
-            children: saleDocumentData?.issueDateTimeString
-        }
-    ];
+  const { id, saleDocumentData, loading, className } = props;
+  const [modalState, setModalState] = useState(false);
+  const [modalContent, setModalContent] = useState(null);
+  const title = `${saleDocumentData?.saleDocumentType} شماره ${saleDocumentData?.documentNumber}`;
+  const referenceDocumentNumber =
+    saleDocumentData?.referenceDocumentNumber !== 0 ? (
+      <Typography.Link href="#">{`${saleDocumentData?.referenceDocumentType} شماره ${saleDocumentData?.referenceDocumentNumber}`}</Typography.Link>
+    ) : (
+      "-"
+    );
+  const borderedItems = [
+    {
+      key: "1",
+      label: "شماره سریال",
+      children: saleDocumentData?.serialNumber,
+    },
+    {
+      key: "2",
+      label: "برگه مرجع",
+      children: referenceDocumentNumber,
+    },
+    {
+      key: "3",
+      label: "کانال فروش",
+      children: saleDocumentData?.saleChannel,
+    },
+    {
+      key: "4",
+      label: "نام شعبه",
+      children: saleDocumentData?.branchName,
+    },
+    {
+      key: "6",
+      label: "مشتری",
+      children: (
+        <Typography.Link
+          onClick={() => onViewCustomer(saleDocumentData.customerId)}
+        >
+          {saleDocumentData?.customerName}
+        </Typography.Link>
+      ), //customerId
+    },
+    {
+      key: "5",
+      label: "زمان صدور",
+      children: saleDocumentData?.issueDateTimeString,
+    },
+  ];
 
-    //====================================================================
-    //                        Component
-    //====================================================================
-    return (
-        <>
-            <Ant.Skeleton loading={loading}>
-                <Ant.Descriptions 
-                    bordered={false}
-                    layout="horizontal"
-                    title={title}
-                    size="small"
-                    items={borderedItems}
-                    className={className}
-                />
-            </Ant.Skeleton>
-        </>
-    )
-}
+  const onViewCustomer = (id) => {
+    console.log(id, "hahahahaj");
+    setModalContent(<CustomerDescription id={id} />);
+
+    setModalState(true);
+  };
+
+  //====================================================================
+  //                        Component
+  //====================================================================
+  return (
+    <>
+      <Ant.Modal
+        {...defaultValues.MODAL_PROPS}
+        open={modalState}
+        centered
+        getContainer={null}
+        footer={null}
+        onCancel={() => {
+          setModalState(false);
+        }}
+        onOk={() => {
+          setModalState(false);
+        }}
+      >
+        {modalContent}
+      </Ant.Modal>
+
+      <Ant.Skeleton loading={loading}>
+        <Ant.Descriptions
+          bordered={false}
+          layout="horizontal"
+          title={title}
+          size="small"
+          items={borderedItems}
+          className={className}
+        />
+      </Ant.Skeleton>
+    </>
+  );
+};
 
 export default SaleDocumentHeaderInfo;
--- src/Pages/salesCommerce/saleDocument/list/SaleDocumentList.jsx
+++ src/Pages/salesCommerce/saleDocument/list/SaleDocumentList.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import qs from "qs";
 import * as Ant from "antd";
 import * as url from "@/api/url";
-import * as styles from "@/styles"
+import * as styles from "@/styles";
 import * as api from "@/api";
 import * as defaultValues from "@/defaultValues";
 import { columns } from "./columns";
@@ -11,158 +11,170 @@ import FilterPanel from "./FilterPanel";
 import FilterBedge from "@/components/common/FilterBedge";
 import FilterDrawer from "@/components/common/FilterDrawer";
 import SaleDocumentDescription from "../description/SaleDocumentDescription";
+import CustomerDescription from "../../basicInformation/CustomerManagement/description/CustomerDescription";
 
 //====================================================================
 //                        Declaration
 //====================================================================
 const SaleDocumentList = () => {
-    const pageTitle = "مدیریت برگه های فروش";
-    const [listData, listLoading, listError, listApiCall] = api.useFetchWithHandler();
-    const [dataSource, setDataSource] = useState(null);
-    const [openFilter, setOpenFilter] = useState(false);
-    const [filterCount, setFilterCount] = useState(0);
-    const [filterObject, setFilterObject] = useState();
-    const [pagination, setPagination] = useState({
-        current: 1,
-        pageSize: 10,
+  const pageTitle = "مدیریت برگه های فروش";
+  const [listData, listLoading, listError, listApiCall] =
+    api.useFetchWithHandler();
+  const [dataSource, setDataSource] = useState(null);
+  const [openFilter, setOpenFilter] = useState(false);
+  const [filterCount, setFilterCount] = useState(0);
+  const [filterObject, setFilterObject] = useState();
+  const [pagination, setPagination] = useState({
+    current: 1,
+    pageSize: 10,
+  });
+  const [documentDetailModalState, setDocumentDetailModalState] =
+    useState(false);
+  const [documentDetailModalContent, setDocumentDetailModalContent] =
+    useState(null);
+  //====================================================================
+  //                        useEffects
+  //====================================================================
+  useEffect(() => {
+    fillGrid();
+  }, [pagination.current, pagination.pageSize]);
+
+  useEffect(() => {
+    setDataSource(listData?.data);
+    setPagination({
+      ...pagination,
+      total: listData?.data[0]?.totalCount,
     });
-    const [documentDetailModalState, setDocumentDetailModalState] = useState(false);
-    const [documentDetailModalContent, setDocumentDetailModalContent] = useState(null);
-    //====================================================================
-    //                        useEffects
-    //====================================================================
-    useEffect(() => {
-        fillGrid();
-    }, [pagination.current, pagination.pageSize]);
+  }, [listData]);
 
-    useEffect(() => {
-        setDataSource(listData?.data);
-        setPagination({
-            ...pagination,
-            total: listData?.data[0]?.totalCount,
-        });
-    }, [listData]);
-    
-    useEffect(() => {
-        setPagination({...pagination, current: 1});
-        filterObject && 
-        setFilterCount(
-            Object.keys(filterObject)?.filter((key) => filterObject[key])?.length,
-        );
-        !filterObject && setFilterCount(0);
-        fillGrid();
-    }, [filterObject]);
-    //====================================================================
-    //                        Functions
-    //====================================================================
-    const fillGrid = async () => {
-        let customerFilter = {};
-        if (filterObject && filterObject.customerId){
-            customerFilter.customerId = filterObject.customerId.value;
-        }
-        else {
-            customerFilter.customerId = undefined;
-        }
-        const queryString = qs.stringify({
-            ...filterObject,
-            ...customerFilter,
-            pageNumber: pagination.current,
-            pageSize: pagination.pageSize
-        });
-        await listApiCall(`${url.SALE_DOCUMENT_HEADER}?${queryString}`);
-    };
-    
-    const onFilterChanged = async(filterObject) => {
-        setFilterObject(filterObject);
-        setOpenFilter(false);
+  useEffect(() => {
+    setPagination({ ...pagination, current: 1 });
+    filterObject &&
+      setFilterCount(
+        Object.keys(filterObject)?.filter((key) => filterObject[key])?.length,
+      );
+    !filterObject && setFilterCount(0);
+    fillGrid();
+  }, [filterObject]);
+  //====================================================================
+  //                        Functions
+  //====================================================================
+  const fillGrid = async () => {
+    let customerFilter = {};
+    if (filterObject && filterObject.customerId) {
+      customerFilter.customerId = filterObject.customerId.value;
+    } else {
+      customerFilter.customerId = undefined;
     }
+    const queryString = qs.stringify({
+      ...filterObject,
+      ...customerFilter,
+      pageNumber: pagination.current,
+      pageSize: pagination.pageSize,
+    });
+    await listApiCall(`${url.SALE_DOCUMENT_HEADER}?${queryString}`);
+  };
 
-    const handleTableChange = (pagination, filters, sorter) => {
-        setPagination(pagination);
-    };
+  const onFilterChanged = async (filterObject) => {
+    setFilterObject(filterObject);
+    setOpenFilter(false);
+  };
 
-    const onRemoveFilter = () => {
-        setFilterObject(null);
-        setOpenFilter(false);
-    };
+  const handleTableChange = (pagination, filters, sorter) => {
+    setPagination(pagination);
+  };
 
-    const onDelete = async (id) => {
-        //TODO: not implemented
-        console.log("onDelete - " + id);
-    };
+  const onRemoveFilter = () => {
+    setFilterObject(null);
+    setOpenFilter(false);
+  };
 
-    const onEdit = async (id) => {
-        //TODO: not implemented
-        console.log("onEdit - " + id);
-    };
+  const onDelete = async (id) => {
+    //TODO: not implemented
+    console.log("onDelete - " + id);
+  };
 
-    const onView = async (id) => {
-        setDocumentDetailModalContent(<SaleDocumentDescription id={id}/>)
-        setDocumentDetailModalState(true);
-    };
+  const onEdit = async (id) => {
+    //TODO: not implemented
+    console.log("onEdit - " + id);
+  };
 
-    //====================================================================
-    //                        Child Components
-    //====================================================================
-    const title = () => {
-        return (
-            <>
-                <ButtonList
-                    filterCount={filterCount}
-                    onAdd={() => console.log("grid-onAdd")}
-                    onRefresh={() => fillGrid()}
-                    onFilter={() => setOpenFilter(true)}
-                />
-            </>
-        );
-    };
-    //====================================================================
-    const Grid = () => {
-        return (
-            <>
-                <Ant.Skeleton loading={listLoading}>
-                    <Ant.Table
-                        columns={columns(onDelete, onEdit, onView)}
-                        dataSource={dataSource}
-                        pagination={pagination}
-                        onChange={handleTableChange}
-                        {...defaultValues.TABLE_PROPS}
-                        title={title}
-                    />
-                </Ant.Skeleton>
-            </>
-        )
-    }
-    //====================================================================
-    //                        Component
-    //====================================================================
+  const onView = async (id) => {
+    setDocumentDetailModalContent(<SaleDocumentDescription id={id} />);
+    setDocumentDetailModalState(true);
+  };
+  const onViewCustomer = async (id) => {
+    setDocumentDetailModalContent(<CustomerDescription id={id} />);
+    setDocumentDetailModalState(true);
+  };
+
+  //====================================================================
+  //                        Child Components
+  //====================================================================
+  const title = () => {
     return (
-        <>
-            <Ant.Modal
-                open={documentDetailModalState}
-                centered
-                width={1200}
-                getContainer={null}
-                footer={null}
-                onCancel={() => setDocumentDetailModalState(false)}
-                onOk = {() => setDocumentDetailModalState(false)}
-            >
-                {documentDetailModalContent}
-            </Ant.Modal>
-            <Ant.Card style={{...styles.CARD_DEFAULT_STYLES}} loading={listLoading} title={pageTitle} type="inner">
-                <FilterDrawer
-                    open={openFilter}
-                    onClose={() => setOpenFilter(false)}
-                    onRemoveFilter={onRemoveFilter}
-                >
-                    <FilterPanel filterObject={filterObject} onSubmit={onFilterChanged} />
-                </FilterDrawer>
-                <FilterBedge filterCount={filterCount}>
-                    <Grid/>
-                </FilterBedge>
-            </Ant.Card>
-        </>
-    )
-}
+      <>
+        <ButtonList
+          filterCount={filterCount}
+          onAdd={() => console.log("grid-onAdd")}
+          onRefresh={() => fillGrid()}
+          onFilter={() => setOpenFilter(true)}
+        />
+      </>
+    );
+  };
+  //====================================================================
+  const Grid = () => {
+    return (
+      <>
+        <Ant.Skeleton loading={listLoading}>
+          <Ant.Table
+            columns={columns(onDelete, onEdit, onView, onViewCustomer)}
+            dataSource={dataSource}
+            pagination={pagination}
+            onChange={handleTableChange}
+            {...defaultValues.TABLE_PROPS}
+            title={title}
+          />
+        </Ant.Skeleton>
+      </>
+    );
+  };
+  //====================================================================
+  //                        Component
+  //====================================================================
+  return (
+    <>
+      <Ant.Modal
+        open={documentDetailModalState}
+        centered
+        {...defaultValues.MODAL_PROPS}
+        getContainer={null}
+        footer={null}
+        onCancel={() => setDocumentDetailModalState(false)}
+        onOk={() => setDocumentDetailModalState(false)}
+      >
+        {documentDetailModalContent}
+      </Ant.Modal>
+      <Ant.Card
+        style={{ ...styles.CARD_DEFAULT_STYLES }}
+        loading={listLoading}
+        title={pageTitle}
+        type="inner"
+      >
+        <FilterDrawer
+          open={openFilter}
+          onClose={() => setOpenFilter(false)}
+          onRemoveFilter={onRemoveFilter}
+        >
+          <FilterPanel filterObject={filterObject} onSubmit={onFilterChanged} />
+        </FilterDrawer>
+        <FilterBedge filterCount={filterCount}>
+          <Grid />
+        </FilterBedge>
+      </Ant.Card>
+    </>
+  );
+};
 
 export default SaleDocumentList;
