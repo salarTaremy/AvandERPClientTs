fb04680372da8bceb99ed3ee33db4e0e44d3c806
zand23 <52084239+zand23@users.noreply.github.com>
2024-05-25 09:59:12 +0330

    ok

--- src/Pages/salesCommerce/saleDocument/description/SaleDocumentDescription.jsx
+++ src/Pages/salesCommerce/saleDocument/description/SaleDocumentDescription.jsx
@@ -10,20 +10,31 @@ import useRequestManager from "@/hooks/useRequestManager";
 //                        Declaration
 //====================================================================
 const SaleDocumentDescription = (props) => {
-    const { id } = props;
-    const [saleDocumentData, saleDocumentLoading, saleDocumentError] = api.useFetch(`${url.SALE_DOCUMENT_HEADER}/${id}`);
-    const saleDocumentFetchedData = saleDocumentData?.data;
-    useRequestManager({ error: saleDocumentError });
-    //====================================================================
-    //                        Component
-    //====================================================================
-    return (
-        <>
-            <SaleDocumentHeaderInfo id={id} saleDocumentData={saleDocumentFetchedData} loading={saleDocumentLoading} className="pb-5"/>
-            <SaleDocumentDetail saleDocumentHeaderId={id}/>
-            <SaleDocumentFooterInfo id={id} saleDocumentData={saleDocumentFetchedData} loading={saleDocumentLoading} className="pt-5"/>
-        </>
-    )
-}
+  const { id } = props;
+  const [saleDocumentData, saleDocumentLoading, saleDocumentError] =
+    api.useFetch(`${url.SALE_DOCUMENT_HEADER}/${id}`);
+  const saleDocumentFetchedData = saleDocumentData?.data;
+  useRequestManager({ error: saleDocumentError });
+  //====================================================================
+  //                        Component
+  //====================================================================
+  return (
+    <>
+      <SaleDocumentHeaderInfo
+        id={id}
+        saleDocumentData={saleDocumentFetchedData}
+        loading={saleDocumentLoading}
+        className="pb-5"
+      />
+      <SaleDocumentDetail saleDocumentHeaderId={id} />
+      <SaleDocumentFooterInfo
+        id={id}
+        saleDocumentData={saleDocumentFetchedData}
+        loading={saleDocumentLoading}
+        className="pt-5"
+      />
+    </>
+  );
+};
 
 export default SaleDocumentDescription;
