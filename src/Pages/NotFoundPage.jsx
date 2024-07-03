a6ad5958a3bd54a850fdffe66240b83a0f9e518b
salar taremi <Salar_taremy@yahoo.com>
2024-07-03 16:23:46 +0330

    ok

--- src/Pages/NotFoundPage.jsx
+++ src/Pages/NotFoundPage.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import * as Ant from "antd";
+import { Col, Divider, Row } from 'antd';
+import CoustomContent from "@/components/common/CoustomContent";
 const NotFoundPage = () => {
   return (
-    <Ant.Card>
-      <Ant.Result
-    status="404"
-    title="404"
-    subTitle="اوه... این صفحه وجود نداره !"
-  />
-    </Ant.Card>
+    <>
+      <CoustomContent Height="85vh" Shadow>
+        <Ant.Result
+          status="404"
+          title="404"
+          subTitle="اوه... این صفحه وجود نداره !"
+        />
+      </CoustomContent>
+    </>
   );
 };
 export default NotFoundPage;
