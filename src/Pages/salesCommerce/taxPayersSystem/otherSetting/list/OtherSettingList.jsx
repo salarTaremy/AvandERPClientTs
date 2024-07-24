066cb48483424b8655814b0cd6b4e112e121f390
mohebali92 <157206069+mohebali92@users.noreply.github.com>
2024-07-24 16:45:41 +0330

    TaxPayersSystem: Setting managment was implemented.

--- src/Pages/salesCommerce/taxPayersSystem/otherSetting/list/OtherSettingList.jsx
+++ src/Pages/salesCommerce/taxPayersSystem/otherSetting/list/OtherSettingList.jsx
@@ -1,179 +1,114 @@
 import React from 'react'
 import * as Ant from "antd";
 import { useEffect, useState } from "react";
-import columns from "./columns";
-import * as defaultValues from "@/defaultValues";
 import * as styles from "@/styles";
 import * as url from "@/api/url";
-import qs from "qs";
-import ButtonList from "@/components/common/ButtonList";
 import useRequestManager from "@/hooks/useRequestManager";
-import { useFetchWithHandler, useDelWithHandler } from "@/api";
-import * as uuid from "uuid";
-import FormAddOtherSetting from '../add/FormAddOtherSetting';
-import FormEditOtherSetting from '../edit/FormEditOtherSetting';
-import FilterDrawer from "@/components/common/FilterDrawer";
-import FilterPanel from './FilterPanel';
-import FilterBedge from "@/components/common/FilterBedge";
+import { useFetchWithHandler, usePutWithHandler } from "@/api";
 
 
 const OtherSettingList = () => {
     const [listData, loading, error, ApiCall] = useFetchWithHandler();
-    const [delSaving, delLoading, delError, delApiCall] = useDelWithHandler();
-    const [dataSource, setDataSource] = useState(null);
+    const [editData, editLoading, editError, editApiCall] = usePutWithHandler()
+    useRequestManager({ error: editError, loading: editLoading, data: editData })
     useRequestManager({ error: error });
-    useRequestManager({ error: delError, loading: delLoading, data: delSaving });
-    const [modalState, setModalState] = useState(false);
-    const [modalContent, setModalContent] = useState();
-    const [pagination, setPagination] = useState({});
-    const [openFilter, setOpenFilter] = useState(false);
-    const [filterCount, setFilterCount] = useState(0);
-    const [filterObject, setFilterObject] = useState();
+    const [defaultValue, setDefaultValue] = useState()
+    const [selectedSchema, setSelectedSchema] = useState()
+    const [form] = Ant.Form.useForm()
 
     //====================================================================
     //                        useEffects
     //====================================================================
     useEffect(() => {
-        filterObject &&
-            setFilterCount(
-                Object.keys(filterObject)?.filter((key) => filterObject[key])?.length,
-            );
-        !filterObject && setFilterCount(0);
-        getAllOtherSetting();
-    }, [filterObject]);
+        getAllOtherSetting()
+    }, [])
 
     useEffect(() => {
-        setDataSource((listData?.isSuccess && listData?.data) || null);
-    }, [listData]);
-
-    useEffect(() => {
-        delSaving?.isSuccess &&
-            setDataSource([
-                ...dataSource?.filter((c) => c.id !== delSaving?.data?.id),
-            ]);
-    }, [delSaving]);
+        form.resetFields()
+        listData?.isSuccess && form.setFieldsValue({ ...(listData?.data || null) })
+        const urlSchema = listData?.data?.apiURL.split("://");
+        listData?.isSuccess && form.setFieldsValue({ apiURL: `${urlSchema[1]}` })
+        urlSchema && setDefaultValue(`${urlSchema[0]}://`);
+        urlSchema && setSelectedSchema(`${urlSchema[0]}://`);
+    }, [listData])
 
     //====================================================================
     //                        Functions
     //====================================================================
     const getAllOtherSetting = async () => {
-        const queryString = qs.stringify(filterObject);
-        await ApiCall(`${url.TPS_CONFIG}?${queryString}`);
+        await ApiCall(url.TPS_CONFIG);
     };
 
+    const onFinish = async (values) => {
+        const req = {
+            ...values,
+            apiURL: `${selectedSchema}${values.apiURL}`
+        }
+        await editApiCall(url.TPS_CONFIG, req)
+    }
 
-    const onFilterChanged = async (filterObject) => {
-        setFilterObject(filterObject);
-        setOpenFilter(false);
-    };
+    const addonAfter = (
+        <Ant.Select defaultValue={defaultValue} onChange={(value) => setSelectedSchema(value)}>
+            <Option value="http://">://http</Option>
+            <Option value="https://">://https</Option>
+        </Ant.Select>
+    )
 
-    const onRemoveFilter = () => {
-        setFilterObject(null);
-        setOpenFilter(false);
-    };
-
-    const onDelete = async (id) => {
-        await delApiCall(`${url.TPS_CONFIG}/${id}`);
-    };
-
-    const onSuccessEdit = () => {
-        setModalState(false);
-        getAllOtherSetting();
-    };
-
-    const onAdd = () => {
-        setModalContent(
-            <FormAddOtherSetting key={uuid.v1()} onSuccess={onSuccessAdd} />
-        );
-        setModalState(true);
-    };
-
-    const onSuccessAdd = () => {
-        setModalState(false);
-        getAllOtherSetting();
-    };
-
-    const handleTableChange = (pagination) => {
-        setPagination(pagination);
-    };
-
-    //====================================================================
-    //                        Events
-    //====================================================================
-    const onEdit = (val) => {
-        setModalContent(
-            <FormEditOtherSetting
-                onSuccess={onSuccessEdit}
-                key={val.id}
-                id={val.id}
-            />
-        );
-        setModalState(true);
-    };
-    //====================================================================
-    //                        Child Components
-    //====================================================================
-    const title = () => {
-        return (
-            <ButtonList
-                filterCount={filterCount}
-                onAdd={() => {
-                    onAdd();
-                }}
-                onFilter={() => {
-                    setOpenFilter(true);
-                }}
-                onRefresh={() => {
-                    getAllOtherSetting();
-                }}
-            />
-        );
-    };
-
-    const Grid = () => {
-        return (
-            <>
-                <Ant.Skeleton loading={loading}>
-                    <Ant.Table
-                        size="small"
-                        {...defaultValues.TABLE_PROPS}
-                        pagination={pagination}
-                        title={title}
-                        columns={columns(onDelete, onEdit)}
-                        onChange={handleTableChange}
-                        dataSource={dataSource}
-                    />
-                </Ant.Skeleton>
-            </>
-        );
-    };
     //====================================================================
     //                        Component
     //====================================================================
     return (
         <>
-            <Ant.Modal
-                open={modalState}
-                handleCancel={() => setModalState(false)}
-                onCancel={() => {
-                    setModalState(false);
-                }}
-                footer={null}
-                centered
-            >
-                {modalContent}
-            </Ant.Modal>
             <Ant.Card style={{ ...styles.CARD_DEFAULT_STYLES }} title={"سایر تنظیمات"} type="inner" loading={loading}>
-                <FilterDrawer
-                    open={openFilter}
-                    onClose={() => setOpenFilter(false)}
-                    onRemoveFilter={onRemoveFilter}
-                >
-                    <FilterPanel filterObject={filterObject} onSubmit={onFilterChanged} />
-                </FilterDrawer>
-                <FilterBedge filterCount={filterCount}>
-                    <Grid />
-                </FilterBedge>
+                <Ant.Form form={form} onFinish={onFinish} >
+                    <Ant.Form.Item
+                        label={"زمان تکرار چرخه ارسال خودکار صورتحساب ها"}
+                        name="sendRecurTimeInMinute"
+                        rules={[{
+                            required: true,
+                            pattern: new RegExp("^[0-9]*$"),
+                            message: 'لطفا مقدار عددی وارد نمایید.'
+                        }]}
+                        style={{ width: 430 }}
+                    >
+                        <Ant.Input suffix="دقیقه" />
+                    </Ant.Form.Item>
+                    <Ant.Form.Item
+                        name="inquiryRecurTimeInMinute"
+                        label={"زمان تکرار چرخه استعلام خودکار صورتحساب ها"}
+                        rules={[{
+                            required: true,
+                            pattern: new RegExp("^[0-9]*$"),
+                            message: 'لطفا مقدار عددی وارد نمایید.'
+                        }]}
+                        style={{ width: 430 }}
+                    >
+                        <Ant.Input suffix="دقیقه" />
+                    </Ant.Form.Item>
+                    <Ant.Form.Item
+                        name="apiURL"
+                        label={"آدرس URL "}
+                        rules={[{
+                            required: true,
+                            pattern: new RegExp("(www.)?[a-zA-Z0-9@:%._\\+~#?&//=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%._\\+~#?&//=]*)"),
+                            message: 'لطفا آدرس URL صحیح وارد نمایید. '
+                        }]}
+                        style={{ width: 430 }}
+                    >
+                        <Ant.Input addonAfter={addonAfter} style={{ textAlign: "left" }} />
+                    </Ant.Form.Item>
+                    <Ant.Form.Item>
+                        <Ant.Button
+                            type="primary"
+                            loading={loading}
+                            onClick={() => {
+                                form.submit();
+                            }}
+                        >
+                            {"ذخیره"}
+                        </Ant.Button>
+                    </Ant.Form.Item>
+                </Ant.Form>
             </Ant.Card>
         </>
     );
