ebd282984db8c026d58af323a22d838d4f671ad5
salar taremi <Salar_taremy@yahoo.com>
2024-08-07 11:17:56 +0330

    Add More Menu in tabless

--- src/Pages/generalSettings/user/list/columns.jsx
+++ src/Pages/generalSettings/user/list/columns.jsx
@@ -2,21 +2,69 @@ import React from 'react'
 import * as Ant from 'antd'
 import { RiDeleteBin6Line } from 'react-icons/ri'
 import { FiEdit } from "react-icons/fi";
+import { GrView } from "react-icons/gr";
 import { VscKey } from "react-icons/vsc";
 import { VscGithubAction } from "react-icons/vsc";
 import { AiOutlineEllipsis } from "react-icons/ai";
+import { CgMoreVertical , CgMore} from "react-icons/cg";
+import { BsMotherboard } from "react-icons/bs";
+
 
 const columns = (onDelete, onEdit, onReset, onInfo, onSwitch, onOtherAccesses) => {
+    const getMenuItems = (val) => [
+        {
+            key: '1',
+            label: (
+                <Ant.Tooltip placement="right" title={'ویرایش'}>
+                    <a onClick={() => onEdit(val)}><FiEdit className="text-blue-600" /></a>
+                </Ant.Tooltip>
+            ),
+        },
+        {
+            key: '2',
+            label: (
+                <Ant.Tooltip placement="right" title={'تغییر رمز عبور'}>
+                    <a onClick={() => onReset(val)}><VscKey className="text-rose-600" /></a>
+                </Ant.Tooltip>
+            ),
+        },
+        {
+            key: '3',
+            label: (
+                <Ant.Tooltip placement="right" title={'لیست نقش ها'}>
+                    <a onClick={() => onInfo(val)}><VscGithubAction className="text-fuchsia-600" /></a>
+                </Ant.Tooltip>
+            ),
+        },
+        {
+            key: '4',
+            label: (
+                <Ant.Tooltip placement="right" title={'ویرایش نقش ها'}>
+                    <a onClick={() => onSwitch(val)}><VscGithubAction className="text-violet-600" /></a>
+                </Ant.Tooltip>
+            ),
+        },
+        {
+            key: '5',
+            label: (
+                <Ant.Tooltip placement="right" title={' سایر دسترسی ها '}>
+                    <a onClick={() => onOtherAccesses(val)}><BsMotherboard className="text-cyan-600" /></a>
+                </Ant.Tooltip>
+            ),
+        },
+    ];
+
+
     return (
         [
-            // {
-            //     title: 'شناسه',
-            //     dataIndex: 'id',
-            //     key: 'id',
-            //     width: 80,
-            //     align: 'center',
-            //     className:"text-xs sm:text-sm",
-            // },
+            {
+                title: 'شناسه کاربری',
+                dataIndex: 'id',
+                key: 'id',
+                width: 80,
+                align: 'center',
+                className: "text-xs sm:text-sm",
+            },
             {
                 title: 'تاریخ',
                 dataIndex: 'createDate',
@@ -62,46 +110,30 @@ const columns = (onDelete, onEdit, onReset, onInfo, onSwitch, onOtherAccesses) =
                 className: "text-xs sm:text-sm",
                 render: (text, val) => (
                     <>
-                        <Ant.Tooltip placement="top" title={' سایر دسترسی ها '}>
+
+                        <Ant.Dropdown
+                            menu={{
+                                items: getMenuItems(val),
+                            }}
+                            placement="bottom"
+                            arrow
+                        >
                             <Ant.Button
-                                className="text-cyan-600"
-                                onClick={() => onOtherAccesses(val)}
-                                icon={<AiOutlineEllipsis />}
+                                onClick={() => { }}
+                                className="text-blue-600"
+                                icon={<CgMoreVertical />}
+                                type="text"
+                            />
+                        </Ant.Dropdown>
+                        <Ant.Tooltip placement="top" title={'جزئیات کاربر'}>
+                            <Ant.Button
+                                onClick={() => { }}
+                                className="text-sky-600"
+                                icon={<GrView />}
                                 type="text"
                             />
                         </Ant.Tooltip>
-                        <Ant.Tooltip placement="top" title={'ویرایش نقش ها'}>
-                            <Ant.Button
-                                className="text-violet-600"
-                                onClick={() => onSwitch(val)}
-                                icon={<VscGithubAction />}
-                                type="text"
-                            />
-                        </Ant.Tooltip>
-                        <Ant.Tooltip placement="top" title={'لیست نقش ها'}>
-                            <Ant.Button
-                                className="text-fuchsia-600"
-                                onClick={() => onInfo(val)}
-                                icon={<VscGithubAction />}
-                                type="text"
-                            />
-                        </Ant.Tooltip>
-                        <Ant.Tooltip placement="top" title={'تغییر رمز عبور'}>
-                            <Ant.Button
-                                className="text-rose-400"
-                                onClick={() => onReset(val)}
-                                icon={<VscKey />}
-                                type="text"
-                            />
-                        </Ant.Tooltip>
-                        <Ant.Tooltip placement="top" title={'ویرایش'}>
-                            <Ant.Button
-                                className="text-blue-500"
-                                onClick={() => onEdit(val)}
-                                icon={<FiEdit />}
-                                type="text"
-                            />
-                        </Ant.Tooltip>
+
                         <Ant.Popconfirm onConfirm={() => onDelete(val.id)} title={`برای حذف کاربر "${val.userName}" مطمئن هستید؟`}>
                             <Ant.Tooltip placement="top" title={'حذف'}>
                                 <Ant.Button className="text-rose-600" icon={<RiDeleteBin6Line />} type="text" />
