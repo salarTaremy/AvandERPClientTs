46546d5cfa7813ab5739fc8ede287d0b24ba33a0
salar taremi <Salar_taremy@yahoo.com>
2024-08-13 12:25:19 +0330

    part 1

--- src/Pages/salesCommerce/saleDocument/edit/EditSaleDoc.jsx
+++ src/Pages/salesCommerce/saleDocument/edit/EditSaleDoc.jsx
@@ -1,6 +1,7 @@
 
 import React, { useEffect, useState } from 'react'
 import { PropTypes } from 'prop-types'
+import { Col, Row } from 'antd';
 import * as Ant from 'antd'
 import * as url from '@/api/url'
 import * as defaultValues from "@/defaultValues";
@@ -54,44 +55,44 @@ const EditSaleDoc = (props) => {
   const c = [{
     title: "productCode",
     dataIndex: "productCode",
-    width:100
+    width: 100
   }, {
     title: "productName",
     dataIndex: "productName",
-    width:100
+    width: 500
   }
     , {
     title: "productUnitId",
     dataIndex: "productUnitId",
-    width:100
+    width: 100
   }, {
     title: "productUnitName",
     dataIndex: "productUnitName",
-    width:100
+    width: 100
   }, {
     title: "batchNumber",
     dataIndex: "batchNumber",
-    width:100
+    width: 100
   }, {
     title: "quantity",
     dataIndex: "quantity",
-    width:100
+    width: 100
   }, {
     title: "unitPrice",
     dataIndex: "unitPrice",
-    width:100
+    width: 100
   }, {
     title: "discount",
     dataIndex: "discount",
-    width:100
+    width: 100
   }, {
     title: "taxTotal",
     dataIndex: "taxTotal",
-    width:100
+    width: 100
   }, {
     title: "consumerPrice",
     dataIndex: "consumerPrice",
-    width:100
+    width: 100
   }
 
   ]
@@ -99,25 +100,38 @@ const EditSaleDoc = (props) => {
   return (
     <>
       <CoustomContent height={700}   >
-          <CoustomContent bordered  >
-            {'نام مشتری'}
-            <br />
-            {'نام مشتری'}
-            {/* <pre>
-    {dataSource && JSON.stringify(dataSource[0], null, "\t")}
-  </pre> */}
-          </CoustomContent>
-
-          <Ant.Table
-            {...defaultValues.TABLE_PROPS}
-            columns={c}
-            loading={itemsLoading}
-            dataSource={dataSource} />
-          <CoustomContent bordered   >
-            <p  >
-            {'جمع'}
-            </p>
-          </CoustomContent>
+        <Row gutter={[16, 16]} >
+          <Col span={24}>
+            <CoustomContent bordered  >
+              {'نام مشتری'} {'نام مشتری'}
+              <br />
+              {'نام مشتری'}
+              {/* <pre> {dataSource && JSON.stringify(dataSource[0], null, "\t")} </pre> */}
+            </CoustomContent>
+          </Col>
+          <Col span={24}>
+            <Ant.Table
+              {...defaultValues.TABLE_PROPS}
+              columns={c}
+              loading={itemsLoading}
+              dataSource={dataSource} />
+          </Col>
+        </Row>
+        <Row align="middle"
+        gutter={[16, 16]}
+      className='absolute inset-x-0 bottom-0'
+        >
+          <Col sm={24} lg={12} xl={12}>
+            <CoustomContent bordered   >
+              {'مبلغ:'}<br></br>{'تخفیف:'}<br></br>{'مالیات'}<br></br>{'درصد تخفیف'}<br></br>{'مبلغ نهایی'}
+            </CoustomContent>
+          </Col>
+          <Col span={24} md={12} >
+            <CoustomContent bordered   >
+              {'جمع'}
+            </CoustomContent>
+          </Col>
+        </Row>
       </CoustomContent>
     </>
 
