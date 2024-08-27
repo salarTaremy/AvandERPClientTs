a5f7f794b114877f82f2c8f9ce494821b798d188
zand23 <52084239+zand23@users.noreply.github.com>
2024-08-27 08:01:16 +0330

    add fild acc

--- src/Pages/salesCommerce/basicInformation/saleType/add/FormAddSaleType.jsx
+++ src/Pages/salesCommerce/basicInformation/saleType/add/FormAddSaleType.jsx
@@ -1,89 +1,141 @@
-import React, { useEffect, useState } from 'react'
-import * as Ant from 'antd'
-import {
-    usePostWithHandler,
-    useFetch
-} from '@/api'
-import useRequestManager from '@/hooks/useRequestManager'
-import PropTypes from 'prop-types'
-import * as url from '@/api/url'
+import React, { useEffect, useState } from "react";
+import * as Ant from "antd";
+import { usePostWithHandler, useFetch } from "@/api";
+import useRequestManager from "@/hooks/useRequestManager";
+import PropTypes from "prop-types";
+import * as url from "@/api/url";
 import ModalHeader from "@/components/common/ModalHeader";
 import { SiAnytype } from "react-icons/si";
 const FormAddSaleType = (props) => {
-    const { onSuccess } = props
-    const [loading, setLoading] = useState(false)
-    const [addData, addLoading, addError, addApiCall] = usePostWithHandler()
-    useRequestManager({ error: addError, loading: addLoading, data: addData })
-    const [currencyData, currencyLoading, currencyError] = useFetch(url.CURRENCY);
-    useRequestManager({ error: currencyError });
-    const [form] = Ant.Form.useForm()
+  const { onSuccess } = props;
+  const [loading, setLoading] = useState(false);
+  const [addData, addLoading, addError, addApiCall] = usePostWithHandler();
+  const [currencyData, currencyLoading, currencyError] = useFetch(url.CURRENCY);
+  const [form] = Ant.Form.useForm();
+  const [accountList, accountLoading, accountError] = useFetch(url.ACCOUNT);
+  const [dtAccData, dtAccLoading, dtAccError] = useFetch(url.DETAILED_ACCOUNT);
+  useRequestManager({ error: currencyError });
+  useRequestManager({ error: dtAccError });
+  useRequestManager({ error: accountError });
+  useRequestManager({ error: addError, loading: addLoading, data: addData });
+  const commonOptions = {
+    showSearch: true,
+    filterOption: (input, option) => option.persianTitle.indexOf(input) >= 0,
+  };
+  const commonOptionsAcc = {
+    placeholder: "انتخاب کنید...",
+    showSearch: true,
+    filterOption: (input, option) =>  option.name.toLowerCase().includes(input.toLowerCase()),
+  };
+  //====================================================================
+  //                        useEffects
+  //====================================================================
+  useEffect(() => {
+    form.setFieldValue("isActive", true);
+  }, [form]);
 
-    const commonOptions = {
-        showSearch: true,
-        filterOption: (input, option) => option.persianTitle.indexOf(input) >= 0,
-    };
-    //====================================================================
-    //                        useEffects
-    //====================================================================
-    useEffect(() => {
-        form.setFieldValue('isActive', true)
-    }, [form])
+  useEffect(() => {
+    addData?.isSuccess && onSuccess();
+  }, [addData]);
+  //====================================================================
+  //                        Functions
+  //====================================================================
+  const onFinish = async (values) => {
+    setLoading(true);
+    const req = { ...values };
+    await addApiCall(url.SALETYPE, req);
+    setLoading(false);
+  };
+  //====================================================================
+  //                        Component
+  //====================================================================
+  return (
+    <>
+      <ModalHeader title={"ایجاد نوع فروش  "} icon={<SiAnytype />} />
 
-    useEffect(() => {
-        addData?.isSuccess && onSuccess()
-    }, [addData])
-    //====================================================================
-    //                        Functions
-    //====================================================================
-    const onFinish = async (values) => {
-        setLoading(true)
-        const req = { ...values }
-        await addApiCall(url.SALETYPE, req)
-        setLoading(false)
-    }
-    //====================================================================
-    //                        Component
-    //====================================================================
-    return (
-        <>
-            <ModalHeader title={'ایجاد نوع فروش  '} icon={<SiAnytype />} />
+      <Ant.Form form={form} onFinish={onFinish} layout="vertical">
+        <Ant.Row gutter={[8, 8]}>
+          <Ant.Col span={24} md={24} lg={24}>
+            <Ant.Form.Item
+              name="title"
+              label={"عنوان فروش"}
+              rules={[{ required: true }]}
+            >
+              <Ant.Input allowClear showCount maxLength={200} />
+            </Ant.Form.Item>
+          </Ant.Col>
+          <Ant.Col span={24} md={24} lg={24}>
+            <Ant.Form.Item
+              name={"defaultCurrencyId"}
+              label="نام ارز"
 
-            <Ant.Form form={form} onFinish={onFinish} layout="vertical">
+            >
+              <Ant.Select
+                {...commonOptions}
+                allowClear={true}
+                placeholder={"انتخاب کنید..."}
+                disabled={currencyLoading || false}
+                loading={currencyLoading}
+                options={currencyData?.data}
+                fieldNames={{ label: "persianTitle", value: "id" }}
+              />
+            </Ant.Form.Item>
+          </Ant.Col>
+          <Ant.Col span={24} md={24} lg={24}>
+            <Ant.Form.Item
+              name={"accountId"}
+              label="حساب "
 
-                <Ant.Form.Item name="title" label={'عنوان فروش'} rules={[{ required: true }]}>
-                    <Ant.Input allowClear showCount maxLength={200} />
-                </Ant.Form.Item>
-                <Ant.Form.Item
-                    name={"defaultCurrencyId"}
-                    label="نام ارز"
-                    valuePropName="checked"
-                >
-                    <Ant.Select
-                        {...commonOptions}
-                        allowClear={true}
-                        placeholder={"انتخاب کنید..."}
-                        disabled={currencyLoading || false}
-                        loading={currencyLoading}
-                        options={currencyData?.data}
-                        fieldNames={{ label: "persianTitle", value: "id" }}
-                    />
-                </Ant.Form.Item>
-                <Ant.Form.Item>
-                    <Ant.Button block
-                        type="primary"
-                        loading={loading}
-                        onClick={() => {
-                            form.submit()
-                        }}>
-                        {'تایید'}
-                    </Ant.Button>
-                </Ant.Form.Item>
-            </Ant.Form>
-        </>
-    )
-}
+            >
+              <Ant.Select
+                {...commonOptionsAcc}
+                allowClear={true}
+                placeholder={"انتخاب کنید..."}
+                disabled={accountLoading || false}
+                loading={accountLoading}
+                options={accountList?.data}
+                fieldNames={{ label: "name", value: "id" }}
+              />
+            </Ant.Form.Item>
+          </Ant.Col>
+          <Ant.Col span={24} md={24} lg={24}>
+            <Ant.Form.Item
+              name={"detailedAccountId"}
+              label="حساب تفصیلی"
 
-export default FormAddSaleType
+            >
+              <Ant.Select
+                  {...commonOptionsAcc}
+                allowClear={true}
+                placeholder={"انتخاب کنید..."}
+                disabled={dtAccLoading || false}
+                loading={dtAccLoading}
+                options={dtAccData?.data}
+                fieldNames={{ label: "name", value: "id" }}
+              />
+            </Ant.Form.Item>
+          </Ant.Col>
+          <Ant.Col span={24} md={24} lg={24}>
+            <Ant.Form.Item>
+              <Ant.Button
+                block
+                type="primary"
+                loading={loading}
+                onClick={() => {
+                  form.submit();
+                }}
+              >
+                {"تایید"}
+              </Ant.Button>
+            </Ant.Form.Item>
+          </Ant.Col>
+        </Ant.Row>
+      </Ant.Form>
+    </>
+  );
+};
+
+export default FormAddSaleType;
 FormAddSaleType.propTypes = {
-    onSuccess: PropTypes.func,
-}
+  onSuccess: PropTypes.func,
+};
--- src/Pages/salesCommerce/basicInformation/saleType/edit/FormEditSaleType.jsx
+++ src/Pages/salesCommerce/basicInformation/saleType/edit/FormEditSaleType.jsx
@@ -1,104 +1,148 @@
-import React, { useEffect, useState } from 'react'
-import * as Ant from 'antd'
-import PropTypes from 'prop-types'
-import * as url from '@/api/url'
-import {
-    usePutWithHandler,
-    useFetch,
-    useFetchWithHandler
-} from '@/api'
-import useRequestManager from '@/hooks/useRequestManager'
+import React, { useEffect, useState } from "react";
+import * as Ant from "antd";
+import PropTypes from "prop-types";
+import * as url from "@/api/url";
+import { usePutWithHandler, useFetch, useFetchWithHandler } from "@/api";
+import useRequestManager from "@/hooks/useRequestManager";
 import ModalHeader from "@/components/common/ModalHeader";
 import { SiAnytype } from "react-icons/si";
 const FormEditSaleType = (props) => {
-    const { onSuccess, id } = props
-    const [loading, setLoading] = useState(false)
-    const [editData, editLoading, editError, editApiCall] = usePutWithHandler()
-    const [listData, loadingData, error, ApiCall] = useFetchWithHandler();
-    useRequestManager({ error: editError, loading: editLoading, data: editData })
-    const [currencyData, currencyLoading, currencyError] = useFetch(url.CURRENCY);
-    useRequestManager({ error: currencyError });
-    const [form] = Ant.Form.useForm()
+  const { onSuccess, id } = props;
+  const [loading, setLoading] = useState(false);
+  const [editData, editLoading, editError, editApiCall] = usePutWithHandler();
+  const [listData, loadingData, error, ApiCall] = useFetchWithHandler();
+  const [currencyData, currencyLoading, currencyError] = useFetch(url.CURRENCY);
+  const [accountList, accountLoading, accountError] = useFetch(url.ACCOUNT);
+  const [dtAccData, dtAccLoading, dtAccError] = useFetch(url.DETAILED_ACCOUNT);
+  useRequestManager({ error: error });
+  useRequestManager({ error: accountError });
+  useRequestManager({ error: dtAccError });
+  useRequestManager({ error: currencyError });
+  useRequestManager({ error: editError, loading: editLoading, data: editData });
+  const [form] = Ant.Form.useForm();
+  const commonOptions = {
+    showSearch: true,
+    filterOption: (input, option) => option.persianTitle.indexOf(input) >= 0,
+  };
+  const commonOptionsAcc = {
+    placeholder: "انتخاب کنید...",
+    showSearch: true,
+    filterOption: (input, option) =>  option.name.toLowerCase().includes(input.toLowerCase()),
+  };
 
-    const commonOptions = {
-        showSearch: true,
-        filterOption: (input, option) => option.persianTitle.indexOf(input) >= 0,
-    };
+  //====================================================================
+  //                        useEffects
+  //====================================================================
+  useEffect(() => {
+    getSaleTypeById();
+  }, []);
 
-    //====================================================================
-    //                        useEffects
-    //====================================================================
-    useEffect(() => {
-        getSaleTypeById()
-    }, []);
+  useEffect(() => {
+    form.resetFields();
+    listData?.isSuccess && form.setFieldsValue({ ...(listData?.data || null) });
+  }, [listData]);
+  //=====================================================================
+  //                        Functions
+  //=====================================================================
+  const getSaleTypeById = async () => {
+    await ApiCall(`${url.SALETYPE}/${id}`);
+  };
 
-    useEffect(() => {
-        form.resetFields()
-        listData?.isSuccess && form.setFieldsValue({ ...(listData?.data || null) })
-    }, [listData])
-    //=====================================================================
-    //                        Functions
-    //=====================================================================
-    const getSaleTypeById = async () => {
-        await ApiCall(`${url.SALETYPE}/${id}`);
-    };
+  const onFinish = async (values) => {
+    setLoading(true);
+    const req = { ...values, id: id };
+    await editApiCall(url.SALETYPE, req);
+    setLoading(false);
+    onSuccess();
+  };
+  //====================================================================
+  //                        Component
+  //====================================================================
+  return (
+    <>
+      <ModalHeader title={"ویرایش نوع فروش "} icon={<SiAnytype />} />
+      <Ant.Skeleton active loading={loadingData}>
+        <Ant.Form form={form} onFinish={onFinish} layout="vertical">
+          <Ant.Row gutter={[8, 8]}>
+          <Ant.Col span={24} md={24} lg={24}>
+            <Ant.Form.Item
+              name="title"
+              label={"عنوان فروش"}
+              rules={[{ required: true }]}
+            >
+              <Ant.Input allowClear showCount maxLength={200} />
+            </Ant.Form.Item>
+            </Ant.Col>
+            <Ant.Col span={24} md={24} lg={24}>
+            <Ant.Form.Item
+              name={"defaultCurrencyId"}
+              label="نام ارز"
 
-    const onFinish = async (values) => {
-        setLoading(true)
-        const req = { ...values, id: id }
-        await editApiCall(url.SALETYPE, req)
-        setLoading(false)
-        onSuccess()
-    }
-    //====================================================================
-    //                        Component
-    //====================================================================
-    return (
-        <>
-            <ModalHeader title={'ویرایش نوع فروش '} icon={<SiAnytype />} />
-            <Ant.Skeleton active loading={loadingData}>
-                <Ant.Form form={form} onFinish={onFinish} layout="vertical">
-                    <Ant.Form.Item name="title" label={'عنوان فروش'} rules={[{ required: true }]}>
-                        <Ant.Input allowClear showCount maxLength={200} />
-                    </Ant.Form.Item>
-                    <Ant.Form.Item
-                        name={"defaultCurrencyId"}
-                        label="نام ارز"
-                        valuePropName="checked"
-                        rules={[{ required: true }]}
-                    >
-                        <Ant.Select
-                            {...commonOptions}
-                            allowClear={true}
-                            placeholder={"انتخاب کنید..."}
-                            disabled={currencyLoading || false}
-                            loading={currencyLoading}
-                            options={currencyData?.data}
-                            fieldNames={{ label: "persianTitle", value: "id" }}
-                        />
-                    </Ant.Form.Item>
-                    <Ant.Form.Item>
-                        <Ant.Button block
-                            type="primary"
-                            loading={loading}
-                            onClick={() => {
-                                form.submit()
-                            }}
-                        >
-                            {'تایید'}
-                        </Ant.Button>
-                    </Ant.Form.Item>
-                </Ant.Form>
-            </Ant.Skeleton>
-        </>
-    )
-}
+              rules={[{ required: true }]}
+            >
+              <Ant.Select
+                {...commonOptions}
+                allowClear={true}
+                placeholder={"انتخاب کنید..."}
+                disabled={currencyLoading || false}
+                loading={currencyLoading}
+                options={currencyData?.data}
+                fieldNames={{ label: "persianTitle", value: "id" }}
+              />
+            </Ant.Form.Item>
+            </Ant.Col>
+            <Ant.Col span={24} md={24} lg={24}>
+              <Ant.Form.Item name={"accountId"} label="حساب ">
+                <Ant.Select
+                  {...commonOptionsAcc}
+                  allowClear={true}
+                  placeholder={"انتخاب کنید..."}
+                  disabled={accountLoading || false}
+                  loading={accountLoading}
+                  options={accountList?.data}
+                  fieldNames={{ label: "name", value: "id" }}
+                />
+              </Ant.Form.Item>
+            </Ant.Col>
+            <Ant.Col span={24} md={24} lg={24}>
+              <Ant.Form.Item name={"detailedAccountId"} label="حساب تفصیلی">
+                <Ant.Select
+                  {...commonOptionsAcc}
+                  allowClear={true}
+                  placeholder={"انتخاب کنید..."}
+                  disabled={dtAccLoading || false}
+                  loading={dtAccLoading}
+                  options={dtAccData?.data}
+                  fieldNames={{ label: "name", value: "id" }}
+                />
+              </Ant.Form.Item>
+            </Ant.Col>
+            <Ant.Col span={24} md={24} lg={24}>
+              <Ant.Form.Item>
+                <Ant.Button
+                  block
+                  type="primary"
+                  loading={loading}
+                  onClick={() => {
+                    form.submit();
+                  }}
+                >
+                  {"تایید"}
+                </Ant.Button>
+              </Ant.Form.Item>
+            </Ant.Col>
+          </Ant.Row>
+        </Ant.Form>
+      </Ant.Skeleton>
+    </>
+  );
+};
 
-export default FormEditSaleType
+export default FormEditSaleType;
 FormEditSaleType.propTypes = {
-    onFinish: PropTypes.func,
-    onSuccess: PropTypes.func,
-    obj: PropTypes.any,
-    id: PropTypes.number,
-    loading: PropTypes.bool,
-}
+  onFinish: PropTypes.func,
+  onSuccess: PropTypes.func,
+  obj: PropTypes.any,
+  id: PropTypes.number,
+  loading: PropTypes.bool,
+};
