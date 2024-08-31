8052cc759ce138bf01ed7fb11e480f76b54bc994
salar taremi <37133468+salarTaremy@users.noreply.github.com>
2024-08-31 17:06:22 +0330

    1)Reconfig build Options in vite 2) drop Duplicate Props from two component

--- vite.config.js
+++ vite.config.js
@@ -12,20 +12,20 @@ export default defineConfig({
     },
   },
 
-  build: {
-    chunkSizeWarningLimit: 3000,
-  },
-
   // build: {
-  //   rollupOptions: {
-  //     output: {
-  //       manualChunks(id) {
-  //         if (id.includes('node_modules')) {
-  //           return id.toString().split('node_modules/')[1].split('/')[0].toString();
-  //         }
-  //       }
-  //     }
-  //   }
-  // }
+  //   chunkSizeWarningLimit: 3000,
+  // },
+
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    }
+  }
 
 })
