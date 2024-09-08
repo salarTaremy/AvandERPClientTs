bf0c553ca83629b12d77e2aabe94d69996848546
zand23 <52084239+zand23@users.noreply.github.com>
2024-09-08 15:12:28 +0330

    Fix ACC

--- src/Pages/salesCommerce/basicInformation/deliveryType/add/FormAddDeliveryType.jsx
+++ src/Pages/salesCommerce/basicInformation/deliveryType/add/FormAddDeliveryType.jsx
@@ -1,19 +1,31 @@
 import React, { useEffect, useState } from "react";
 import * as Ant from "antd";
-import { usePostWithHandler, useFetchWithHandler,useFetch } from "@/api";
+import { usePostWithHandler, useFetchWithHandler, useFetch } from "@/api";
 import useRequestManager from "@/hooks/useRequestManager";
 import PropTypes from "prop-types";
 import * as url from "@/api/url";
 import ModalHeader from "@/components/common/ModalHeader";
 import { TbTruckDelivery } from "react-icons/tb";
-
+import * as api from "@/api";
+import useAllLoading from "@/hooks/useAllLoading ";
 const FormAddDeliveryType = (props) => {
   const { onSuccess } = props;
   const [loading, setLoading] = useState(false);
   const [addData, addLoading, addError, addApiCall] = usePostWithHandler();
-  const [accountList, accountLoading, accountError] = useFetch(url.ACCOUNT);
+
   const [dtAccData, dtAccLoading, dtAccError] = useFetch(url.DETAILED_ACCOUNT);
-  useRequestManager({ error: accountError });
+  const [
+    accounGroupTreeData,
+    accounGroupTreeLoading,
+    accounGroupTreeError,
+    accounGroupTreeApicall,
+  ] = api.useFetchWithHandler();
+  const [options, setOptions] = useState([]);
+  const [selectedAccount, setSelectedAccount] = useState({
+    id: null,
+    name: "",
+  });
+  useRequestManager({ error: accounGroupTreeError });
   useRequestManager({ error: dtAccError });
   useRequestManager({ error: addError, loading: addLoading, data: addData });
   const [form] = Ant.Form.useForm();
@@ -23,18 +35,42 @@ const FormAddDeliveryType = (props) => {
     filterOption: (input, option) =>
       option.name.toLowerCase().includes(input.toLowerCase()),
   };
+  const filter = (inputValue, path) =>
+    path.some(
+      (option) =>
+        option.name.toLowerCase().indexOf(inputValue.toLowerCase()) > -1 ||
+        String(option.fullCode).indexOf(inputValue) > -1,
+    );
+  const allLoading = useAllLoading([, accounGroupTreeLoading,dtAccLoading])
   //====================================================================
   //                        useEffects
   //====================================================================
   useEffect(() => {
     addData?.isSuccess && onSuccess();
   }, [addData]);
+
+
+  useEffect(() => {
+    accounGroupTreeData?.isSuccess && setOptions(accounGroupTreeData?.data);
+  }, [accounGroupTreeData]);
+
+  useEffect(() => {
+    accounGroupTreeApicall(url.ACCOUNT_TREE);
+  }, []);
   //====================================================================
   //                        Functions
   //====================================================================
+  const handleChangeAccount = (value, selectedOptions) => {
+    const lastSelectedOption = selectedOptions[selectedOptions.length - 1];
+
+    setSelectedAccount({
+      id: lastSelectedOption.accountId,
+      name: lastSelectedOption.name,
+    });
+  };
   const onFinish = async (values) => {
     setLoading(true);
-    const req = { ...values };
+    const req = { ...values, accountId: selectedAccount.id };
     await addApiCall(url.DELIVERY_TYPE, req);
     setLoading(false);
   };
@@ -45,7 +81,7 @@ const FormAddDeliveryType = (props) => {
   return (
     <>
       <ModalHeader title={"ایجاد نوع تحویل"} icon={<TbTruckDelivery />} />
-
+      <Ant.Skeleton loading={allLoading}>
       <Ant.Form form={form} onFinish={onFinish} layout="vertical">
         <Ant.Row gutter={[8, 8]}>
           <Ant.Col span={24} md={24} lg={24}>
@@ -57,39 +93,63 @@ const FormAddDeliveryType = (props) => {
               <Ant.Input allowClear showCount maxLength={100} />
             </Ant.Form.Item>
           </Ant.Col>
-          <Ant.Col span={24} md={24} lg={24}>
-              <Ant.Form.Item name={"accountId"} label="حساب ">
-                <Ant.Select
-                  {...commonOptionsAcc}
-                  allowClear={true}
-                  placeholder={"انتخاب کنید..."}
-                  disabled={accountLoading || false}
-                  loading={accountLoading}
-                  options={accountList?.data}
-                  fieldNames={{ label: "name", value: "id" }}
-                />
-              </Ant.Form.Item>
-            </Ant.Col>
-            <Ant.Col span={24} md={24} lg={24}>
-              <Ant.Form.Item name={"detailedAccountId"} label="حساب تفصیلی">
-                <Ant.Select
-                  {...commonOptionsAcc}
-                  allowClear={true}
-                  placeholder={"انتخاب کنید..."}
-                  disabled={dtAccLoading || false}
-                  loading={dtAccLoading}
-                  options={dtAccData?.data}
-                  fieldNames={{ label: "name", value: "id" }}
-                />
-              </Ant.Form.Item>
-            </Ant.Col>
+
           <Ant.Col span={24} md={24} lg={24}>
             <Ant.Form.Item
-              name="description"
-              label={"توضیحات"}
-
+              name={"accountId"}
+              label=" حساب "
+              rules={[
+                {
+                  required: true,
+                  message: "فیلد حساب  اجباری است",
+                },
+              ]}
             >
-            <Ant.Input.TextArea allowClear showCount maxLength={400} />
+              <Ant.Cascader
+                loading={accounGroupTreeLoading}
+                options={options}
+                // optionRender={(option) => <span>{option.fullCode +'-'+ option.name}</span> }
+                onChange={handleChangeAccount}
+                placeholder="لطفا انتخاب کنید ..."
+                fieldNames={{
+                  label: "name",
+                  value: "id",
+                  children: "children",
+                }}
+                showSearch={{
+                  filter,
+                }}
+                displayRender={(labels, selectedOptions) => {
+                  const lastLabel = labels[labels.length - 1];
+                  const accountCode =
+                    selectedOptions[selectedOptions.length - 1]?.code;
+
+                  return (
+                    <span>
+                      {lastLabel}
+                      {accountCode && <span> (کد: {accountCode})</span>}
+                    </span>
+                  );
+                }}
+              />
+            </Ant.Form.Item>
+          </Ant.Col>
+          <Ant.Col span={24} md={24} lg={24}>
+            <Ant.Form.Item name={"detailedAccountId"} label="حساب تفصیلی">
+              <Ant.Select
+                {...commonOptionsAcc}
+                allowClear={true}
+                placeholder={"انتخاب کنید..."}
+                disabled={dtAccLoading || false}
+                loading={dtAccLoading}
+                options={dtAccData?.data}
+                fieldNames={{ label: "name", value: "id" }}
+              />
+            </Ant.Form.Item>
+          </Ant.Col>
+          <Ant.Col span={24} md={24} lg={24}>
+            <Ant.Form.Item name="description" label={"توضیحات"}>
+              <Ant.Input.TextArea allowClear showCount maxLength={400} />
             </Ant.Form.Item>
           </Ant.Col>
           <Ant.Col span={24} md={24} lg={24}>
@@ -108,6 +168,7 @@ const FormAddDeliveryType = (props) => {
           </Ant.Col>
         </Ant.Row>
       </Ant.Form>
+      </Ant.Skeleton>
     </>
   );
 };
--- src/Pages/salesCommerce/basicInformation/deliveryType/edit/FormEditDeliveryType.jsx
+++ src/Pages/salesCommerce/basicInformation/deliveryType/edit/FormEditDeliveryType.jsx
@@ -2,50 +2,105 @@ import React, { useEffect, useState } from "react";
 import * as Ant from "antd";
 import PropTypes from "prop-types";
 import * as url from "@/api/url";
-import { usePutWithHandler, useFetchWithHandler,useFetch } from "@/api";
+import { usePutWithHandler, useFetchWithHandler, useFetch } from "@/api";
 import useRequestManager from "@/hooks/useRequestManager";
 import ModalHeader from "@/components/common/ModalHeader";
 import { TbTruckDelivery } from "react-icons/tb";
+import * as api from "@/api";
+import useAllLoading from "@/hooks/useAllLoading ";
 
 const FormEditDeliveryType = (props) => {
   const { onSuccess, obj, id } = props;
   const [loading, setLoading] = useState(false);
   const [editData, editLoading, editError, editApiCall] = usePutWithHandler();
   const [listData, loadingData, error, ApiCall] = useFetchWithHandler();
+  const [accData, accLoading, accError, accApiCall] = useFetchWithHandler();
   useRequestManager({ error: editError, loading: editLoading, data: editData });
+  const [
+    accounGroupTreeData,
+    accounGroupTreeLoading,
+    accounGroupTreeError,
+    accounGroupTreeApicall,
+  ] = api.useFetchWithHandler();
 
-  const [accountList, accountLoading, accountError] = useFetch(url.ACCOUNT);
   const [dtAccData, dtAccLoading, dtAccError] = useFetch(url.DETAILED_ACCOUNT);
   const [form] = Ant.Form.useForm();
-  useRequestManager({ error: accountError });
+
+  const [options, setOptions] = useState([]);
+  const [selectedAccount, setSelectedAccount] = useState({
+    id: null,
+    name: "",
+  });
   useRequestManager({ error: dtAccError });
+  useRequestManager({ error: accError });
+  useRequestManager({ error: error });
+  useRequestManager({ error: accounGroupTreeError });
   const commonOptionsAcc = {
     placeholder: "انتخاب کنید...",
     showSearch: true,
     filterOption: (input, option) =>
       option.name.toLowerCase().includes(input.toLowerCase()),
   };
+  const filter = (inputValue, path) =>
+    path.some(
+      (option) =>
+        option.name.toLowerCase().indexOf(inputValue.toLowerCase()) > -1 ||
+        String(option.fullCode).indexOf(inputValue) > -1,
+    );
+  const allLoading = useAllLoading([
+    loadingData,
+    accounGroupTreeLoading,
+    dtAccLoading,
+  ]);
   //====================================================================
   //                        useEffects
   //====================================================================
   useEffect(() => {
     getDeliveryTypeById();
   }, []);
+  useEffect(() => {
+    accounGroupTreeApicall(url.ACCOUNT_TREE);
+  }, []);
 
   useEffect(() => {
     form.resetFields();
     listData?.isSuccess && form.setFieldsValue({ ...(listData?.data || null) });
   }, [listData]);
+  useEffect(() => {
+    accounGroupTreeData?.isSuccess && setOptions(accounGroupTreeData?.data);
+    accounGroupTreeData?.isSuccess &&
+      accApiCall(`${url.ACCOUNT}/${listData?.data?.accountId}`);
+  }, [accounGroupTreeData]);
+
+  useEffect(() => {
+    if (accData?.isSuccess && accData?.data) {
+      const treeArray = [
+        parseInt(accData.data.accountGroupCode),
+        parseInt(accData.data.parentKey),
+        parseInt(accData.data.key),
+      ];
+      form.setFieldValue("accountId", treeArray);
+    }
+  }, [accData]);
   //=====================================================================
   //                        Functions
   //=====================================================================
+
+  const handleChangeAccount = (value, selectedOptions) => {
+    const lastSelectedOption = selectedOptions[selectedOptions.length - 1];
+
+    setSelectedAccount({
+      id: lastSelectedOption.accountId,
+      name: lastSelectedOption.name,
+    });
+  };
   const getDeliveryTypeById = async () => {
     await ApiCall(`${url.DELIVERY_TYPE}/${id}`);
   };
 
   const onFinish = async (values) => {
     setLoading(true);
-    const req = { ...values, id: id };
+    const req = { ...values, id: id, accountId: selectedAccount.id };
     await editApiCall(url.DELIVERY_TYPE, req);
     setLoading(false);
     onSuccess();
@@ -56,7 +111,7 @@ const FormEditDeliveryType = (props) => {
   return (
     <>
       <ModalHeader title={"ویرایش نوع تحویل"} icon={<TbTruckDelivery />} />
-      <Ant.Skeleton active loading={loadingData}>
+      <Ant.Skeleton active loading={allLoading}>
         <Ant.Form form={form} onFinish={onFinish} layout="vertical">
           <Ant.Row gutter={[8, 8]}>
             <Ant.Col span={24} md={24} lg={24}>
@@ -68,16 +123,47 @@ const FormEditDeliveryType = (props) => {
                 <Ant.Input allowClear showCount maxLength={100} />
               </Ant.Form.Item>
             </Ant.Col>
+
             <Ant.Col span={24} md={24} lg={24}>
-              <Ant.Form.Item name={"accountId"} label="حساب ">
-                <Ant.Select
-                  {...commonOptionsAcc}
-                  allowClear={true}
-                  placeholder={"انتخاب کنید..."}
-                  disabled={accountLoading || false}
-                  loading={accountLoading}
-                  options={accountList?.data}
-                  fieldNames={{ label: "name", value: "id" }}
+              <Ant.Form.Item
+                name={"accountId"}
+                label=" حساب "
+                rules={[
+                  {
+                    required: true,
+                    message: "فیلد حساب  اجباری است",
+                  },
+                ]}
+              >
+                <Ant.Cascader
+                  loading={accounGroupTreeLoading || accLoading}
+                  disabled={accounGroupTreeLoading || accLoading}
+                  options={options}
+                  optionRender={(option) => (
+                    <span>{option.fullCode + "-" + option.name}</span>
+                  )}
+                  onChange={handleChangeAccount}
+                  placeholder="لطفا انتخاب کنید ..."
+                  fieldNames={{
+                    label: "name",
+                    value: "id",
+                    children: "children",
+                  }}
+                  showSearch={{
+                    filter,
+                  }}
+                  displayRender={(labels, selectedOptions) => {
+                    const lastLabel = labels[labels.length - 1];
+                    const accountCode =
+                      selectedOptions[selectedOptions.length - 1]?.fullCode;
+
+                    return (
+                      <span>
+                        {lastLabel}
+                        {accountCode && <span> (کد: {accountCode})</span>}
+                      </span>
+                    );
+                  }}
                 />
               </Ant.Form.Item>
             </Ant.Col>
