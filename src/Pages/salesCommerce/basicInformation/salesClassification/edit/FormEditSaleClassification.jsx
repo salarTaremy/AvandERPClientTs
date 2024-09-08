5dc510bea03f173364de6a831d5bc502ef3220ca
zand23 <52084239+zand23@users.noreply.github.com>
2024-09-08 15:38:03 +0330

    fix active skeleton

--- src/Pages/salesCommerce/basicInformation/salesClassification/edit/FormEditSaleClassification.jsx
+++ src/Pages/salesCommerce/basicInformation/salesClassification/edit/FormEditSaleClassification.jsx
@@ -114,7 +114,7 @@ export const FormEditSaleClassification = (props) => {
   return (
     <>
       <ModalHeader title={"ویرایش طبقه بندی فروش"} icon={<MdGrading />} />
-      <Ant.Skeleton loading={allLoading}>
+      <Ant.Skeleton active loading={allLoading}>
         <Ant.Form form={form} onFinish={onFinish} layout="vertical">
           <Ant.Row gutter={[8, 8]}>
             <Ant.Col span={24} md={24} lg={24}>
