eef759a2be5923c6248b39076b5d317e46461fb8
zand23 <52084239+zand23@users.noreply.github.com>
2024-09-23 11:24:51 +0330

    create page wareHouse sheets

---
+++ src/Pages/inventory/warehouseSheets/add/FormAddTypeWareHouseSheetsList.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const FormAddTypeWareHouseSheetsList = () => {
+  return (
+    <div>FormAddTypeWareHouseSheetsList</div>
+  )
+}
+
+
+export default FormAddTypeWareHouseSheetsList
