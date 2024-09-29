6005b11b7cc1ad0b1cf922d72fb1688a7902e910
zand23 <52084239+zand23@users.noreply.github.com>
2024-09-29 12:37:52 +0330

    filter product kardex

--- src/Pages/inventory/supplier/list/SupplierList.jsx
+++ src/Pages/inventory/supplier/list/SupplierList.jsx
@@ -1,98 +1,108 @@
-import React from 'react'
-import { useEffect, useState } from 'react'
-import * as styles from '@/styles'
-import * as Ant from 'antd'
-import * as url from '@/api/url'
-import qs from 'qs'
-import * as uuid from 'uuid'
-import * as defaultValues from '@/defaultValues'
-import columns from './columns'
-import FormAddSupplier from '../add/FormAddSupplier'
-import FormEditSupplier from '../edit/FormEditSupplier'
-import FilterPanel from './FilterPanel'
-import FilterDrawer from '@/components/common/FilterDrawer'
-import useRequestManager from '@/hooks/useRequestManager'
-import { useFetchWithHandler, useDelWithHandler } from '@/api'
-import FilterBedge from '@/components/common/FilterBedge'
-import ButtonList from '@/components/common/ButtonList'
-import SupplierDescription from '../description/SupplierDescription'
+import React from "react";
+import { useEffect, useState } from "react";
+import * as styles from "@/styles";
+import * as Ant from "antd";
+import * as url from "@/api/url";
+import qs from "qs";
+import * as uuid from "uuid";
+import * as defaultValues from "@/defaultValues";
+import columns from "./columns";
+import FormAddSupplier from "../add/FormAddSupplier";
+import FormEditSupplier from "../edit/FormEditSupplier";
+import FilterPanel from "./FilterPanel";
+import FilterDrawer from "@/components/common/FilterDrawer";
+import useRequestManager from "@/hooks/useRequestManager";
+import { useFetchWithHandler, useDelWithHandler } from "@/api";
+import FilterBedge from "@/components/common/FilterBedge";
+import ButtonList from "@/components/common/ButtonList";
+import SupplierDescription from "../description/SupplierDescription";
 
 const SupplierList = () => {
-  const [listData, loadingData, error, ApiCall] = useFetchWithHandler()
-  const [delSaving, delLoading, delError, delApiCall] = useDelWithHandler()
-  const [openFilter, setOpenFilter] = useState(false)
-  const [modalState, setModalState] = useState(false)
-  const [filterCount, setFilterCount] = useState(0)
-  const [filterObject, setFilterObject] = useState()
-  const [modalContent, setModalContent] = useState()
-  const [dataSource, setDataSource] = useState(null)
-  useRequestManager({ error: error })
-  useRequestManager({ error: delError, data: delSaving, loading: delLoading })
+  const [listData, loadingData, error, ApiCall] = useFetchWithHandler();
+  const [delSaving, delLoading, delError, delApiCall] = useDelWithHandler();
+  const [openFilter, setOpenFilter] = useState(false);
+  const [modalState, setModalState] = useState(false);
+  const [filterCount, setFilterCount] = useState(0);
+  const [filterObject, setFilterObject] = useState();
+  const [modalContent, setModalContent] = useState();
+  const [dataSource, setDataSource] = useState(null);
+  useRequestManager({ error: error });
+  useRequestManager({ error: delError, data: delSaving, loading: delLoading });
   //====================================================================
   //                        useEffects
   //====================================================================
   useEffect(() => {
     filterObject &&
-      setFilterCount(Object.keys(filterObject)?.filter((key) => filterObject[key])?.length)
-    !filterObject && setFilterCount(0)
-    getAllSupplier()
+      setFilterCount(
+        Object.keys(filterObject)?.filter((key) => filterObject[key])?.length,
+      );
+    !filterObject && setFilterCount(0);
+    getAllSupplier();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [filterObject])
+  }, [filterObject]);
 
   useEffect(() => {
-    setDataSource((listData?.isSuccess && listData?.data) || null)
-  }, [listData])
+    setDataSource((listData?.isSuccess && listData?.data) || null);
+  }, [listData]);
   useEffect(() => {
     delSaving?.isSuccess &&
-      setDataSource([...dataSource?.filter((c) => c.id !== delSaving?.data?.id)])
-  }, [delSaving])
+      setDataSource([
+        ...dataSource?.filter((c) => c.id !== delSaving?.data?.id),
+      ]);
+  }, [delSaving]);
   //====================================================================
   //                        Functions
   //====================================================================
 
   const getAllSupplier = async () => {
-    const queryString = qs.stringify(filterObject)
-    await ApiCall(`${url.SUPPLIER}?${queryString}`)
-  }
+    const queryString = qs.stringify(filterObject);
+    await ApiCall(`${url.SUPPLIER}?${queryString}`);
+  };
   const onFilterChanged = async (filterObject) => {
-    setFilterObject(filterObject)
-    setOpenFilter(false)
-  }
+    setFilterObject(filterObject);
+    setOpenFilter(false);
+  };
   const onRemoveFilter = () => {
-    setFilterObject(null)
-    setOpenFilter(false)
-  }
+    setFilterObject(null);
+    setOpenFilter(false);
+  };
   const onAdd = () => {
-    setModalContent(<FormAddSupplier key={uuid.v1()} onSuccess={onSuccessAdd} />)
-    setModalState(true)
-  }
+    setModalContent(
+      <FormAddSupplier key={uuid.v1()} onSuccess={onSuccessAdd} />,
+    );
+    setModalState(true);
+  };
   const onDelete = async (id) => {
-    await delApiCall(`${url.SUPPLIER}/${id}`)
-  }
+    await delApiCall(`${url.SUPPLIER}/${id}`);
+  };
 
   const onSuccessAdd = () => {
-    setModalState(false)
-    getAllSupplier()
-  }
+    setModalState(false);
+    getAllSupplier();
+  };
   const onSuccessEdit = () => {
-    setModalState(false)
-    getAllSupplier()
-  }
+    setModalState(false);
+    getAllSupplier();
+  };
 
   //====================================================================
   //                        Events
   //====================================================================
   const onEdit = (val) => {
-
     setModalContent(
-      <FormEditSupplier myKey={val?.id} obj={val} id={val?.id}  onSuccess={onSuccessEdit} />,
-    )
-    setModalState(true)
-  }
+      <FormEditSupplier
+        myKey={val?.id}
+        obj={val}
+        id={val?.id}
+        onSuccess={onSuccessEdit}
+      />,
+    );
+    setModalState(true);
+  };
   const onView = (id) => {
-    setModalContent(<SupplierDescription id={id} key={id} />)
-    setModalState(true)
-  }
+    setModalContent(<SupplierDescription id={id} key={id} />);
+    setModalState(true);
+  };
   //====================================================================
   //                        Child Components
   //=====================================================================
@@ -103,14 +113,14 @@ const SupplierList = () => {
         filterCount={filterCount}
         onAdd={onAdd}
         onFilter={() => {
-          setOpenFilter(true)
+          setOpenFilter(true);
         }}
         onRefresh={() => {
-          getAllSupplier()
+          getAllSupplier();
         }}
       />
-    )
-  }
+    );
+  };
 
   //====================================================================
   //                        Component
@@ -124,17 +134,20 @@ const SupplierList = () => {
         getContainer={null}
         footer={null}
         onCancel={() => {
-          setModalState(false)
+          setModalState(false);
         }}
         onOk={() => {
-          setModalState(false)
+          setModalState(false);
         }}
       >
         {modalContent}
       </Ant.Modal>
 
-      <Ant.Card style={{ ...styles.CARD_DEFAULT_STYLES }} title={'تأمین کنندگان'} type="inner">
-
+      <Ant.Card
+        style={{ ...styles.CARD_DEFAULT_STYLES }}
+        title={"تأمین کنندگان"}
+        type="inner"
+      >
         <FilterDrawer
           open={openFilter}
           onClose={() => setOpenFilter(false)}
@@ -151,10 +164,8 @@ const SupplierList = () => {
             loading={loadingData}
           />
         </FilterBedge>
-
       </Ant.Card>
-
     </>
-  )
-}
-export default SupplierList
+  );
+};
+export default SupplierList;
--- src/Pages/inventory/wareHouse/reports/productKardex/FilterPanel.jsx
+++ src/Pages/inventory/wareHouse/reports/productKardex/FilterPanel.jsx
@@ -3,17 +3,43 @@ import * as Ant from "antd";
 import { PropTypes } from "prop-types";
 import * as url from "@/api/url";
 import { useFetch } from "@/api";
+import { CheckOutlined, CloseOutlined } from "@ant-design/icons";
+import MyDatePicker from "@/components/common/MyDatePicker";
 import useRequestManager from "@/hooks/useRequestManager";
-
+import * as api from "@/api";
 const FilterPanel = (props) => {
   const { onSubmit, filterObject } = props;
   const [form] = Ant.Form.useForm();
-  const [brandData, brandLoading, brandError] = useFetch(url.BRAND_GET_WITH_PERMISSION);
-  useRequestManager({ error: brandError });
+  //   const [brandData, brandLoading, brandError] = useFetch(
+  //     url.BRAND_GET_WITH_PERMISSION,
+  //   );
+  const [batchNumberList, batchNumberLoading, batchNumberError] = api.useFetch(
+    url.BATCH_NUMBER,
+  );
+  const [wareHouseList, wareHouseLoading, wareHouseError] = api.useFetch(
+    url.WAREHOUSE,
+  );
+  const [
+    inventoryDocumentList,
+    inventoryDocumentLoading,
+    inventoryDocumentError,
+  ] = api.useFetch(url.INVENTORY_DOCUMENT_TYPE);
+  //   useRequestManager({ error: brandError });
+  useRequestManager({ error: batchNumberError });
+  useRequestManager({ error: wareHouseError });
+  useRequestManager({ error: inventoryDocumentError });
 
   const commonOptions = {
     showSearch: true,
-    filterOption: (input, option) => option.name.indexOf(input) >= 0,
+    filterOption: (input, option) => option.batchNumber.indexOf(input) >= 0,
+  };
+  const commonOptionsInventoryDocumentType = {
+    showSearch: true,
+    filterOption: (input, option) => option.title.indexOf(input) >= 0,
+  };
+  const commonOptionsWareHouse = {
+    showSearch: true,
+    filterOption: (input, option) => option.title.indexOf(input) >= 0,
   };
   //====================================================================
   //                        useEffects
@@ -26,8 +52,18 @@ const FilterPanel = (props) => {
   //                        Functions
   //====================================================================
   const onFinish = (values) => {
+    console.log(values, "values");
+    console.log( values?.FromIssueDateCalendarId
+      ?.toString()
+      .replace(/\//g, ""), "ccccc");
     onSubmit({
       ...values,
+      FromIssueDateCalendarId: values?.fromIssueDateCalendarId
+        ?.toString()
+        .replace(/\//g, ""),
+        ToIssueDateCalendarId: values?.ToIssueDateCalendarId
+        ?.toString()
+        .replace(/\//g, ""),
     });
   };
   //====================================================================
@@ -46,62 +82,83 @@ const FilterPanel = (props) => {
         layout="vertical"
         onFinishFailed={null}
       >
-
-        <Ant.Form.Item name={"ProductId"} label=" نام محصول" rules={[{ required: false }]}>
-            <Ant.Cascader
+        <Ant.Form.Item name={"FromIssueDateCalendarId"} label="از تاریخ">
+          <MyDatePicker />
+        </Ant.Form.Item>
+        <Ant.Form.Item name={"ToIssueDateCalendarId"} label="تا تاریخ">
+          <MyDatePicker />
+        </Ant.Form.Item>
+        <Ant.Form.Item
+          name={"ProductId"}
+          label=" نام محصول"
+          rules={[{ required: false }]}
+        >
+          <Ant.Cascader
             //   loading={CityLoading}
             //   options={options}
             //   onChange={onChange}
-              placeholder="لطفا انتخاب کنید ..."
+            placeholder="لطفا انتخاب کنید ..."
             //   fieldNames={{ label: "name", value: "id", children: "children" }}
             //   showSearch={{
             //     filter,
             //   }}
-
-            />
-          </Ant.Form.Item>
-
-        <Ant.Form.Item name={"WarehouseId"} label="نام انبار">
-          <Ant.Input allowClear />
-        </Ant.Form.Item>
-        <Ant.Form.Item name={"InventoryDocumentTypeId"} label=" نوع برگه انبار">
-          <Ant.Input allowClear />
-        </Ant.Form.Item>
-        <Ant.Form.Item name={"FromIssueDateCalendarId"} label="از تاریخ ">
-          <Ant.Input allowClear />
-        </Ant.Form.Item>
-        <Ant.Form.Item name={"FromIssueDateCalendarId"} label="تا تاریخ ">
-          <Ant.Input allowClear />
-        </Ant.Form.Item>
-        <Ant.Form.Item
-          name={"brandId"}
-          label="نام برند"
-          valuePropName="checked"
-        >
-          <Ant.Select
-            {...commonOptions}
-            mode="multiple"
-            allowClear={true}
-            placeholder={"انتخاب کنید..."}
-            disabled={brandLoading}
-            loading={brandLoading}
-            options={brandData?.data}
-            fieldNames={{ label: "name", value: "id" }}
           />
         </Ant.Form.Item>
+        <Ant.Form.Item name={"BatchNumberId"} label="لیست سری ساخت ">
+          <Ant.Select
+            {...commonOptions}
+            disabled={batchNumberLoading}
+            allowClear
+            loading={batchNumberLoading}
+            options={batchNumberList?.data}
+            fieldNames={{ label: "batchNumber", value: "id" }}
+          />
+        </Ant.Form.Item>
+        <Ant.Form.Item name={"WarehouseId"} label="نام انبار">
+          <Ant.Select
+            {...commonOptionsWareHouse}
+            disabled={wareHouseLoading}
+            allowClear
+            loading={wareHouseLoading}
+            options={wareHouseList?.data}
+            fieldNames={{ label: "title", value: "id" }}
+          />
+        </Ant.Form.Item>
+        <Ant.Form.Item name={"InventoryDocumentTypeId"} label="نوع برگه انبار">
+          <Ant.Select
+            {...commonOptionsInventoryDocumentType}
+            allowClear
+            disabled={inventoryDocumentLoading}
+            loading={inventoryDocumentLoading}
+            options={inventoryDocumentList?.data}
+            fieldNames={{ label: "title", value: "id" }}
+          />
+        </Ant.Form.Item>
+
+        <Ant.Form.Item
+          name="IsConfirm"
+          defaultChecked={false}
+          label={"تایید شده/تایید نشده"}
+        >
+          <Ant.Switch
+            checkedChildren={<CheckOutlined />}
+            unCheckedChildren={<CloseOutlined />}
+          />
+        </Ant.Form.Item>
+
         <Ant.Button
           block
           type="primary"
           onClick={() => {
-            form.submit()
+            form.submit();
           }}
         >
-          {'اعمال'}
+          {"اعمال"}
         </Ant.Button>
       </Ant.Form>
     </>
-  )
-}
+  );
+};
 
 export default FilterPanel;
 FilterPanel.propTypes = {
