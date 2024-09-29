1f6687f068119e16751175dae88d4dcfd8d4ccab
salar taremi <37133468+salarTaremy@users.noreply.github.com>
2024-09-29 08:37:38 +0330

    sidebar Icons

--- src/components/SidebarIcon.jsx
+++ src/components/SidebarIcon.jsx
@@ -1,17 +1,27 @@
 import React, { useEffect, useState } from "react";
 import * as Ant from "antd";
 import * as AntIcons from "@ant-design/icons";
+import * as Hi2 from "react-icons/hi2";
+import * as Lia from "react-icons/lia";
+import * as Io5 from "react-icons/io5";
 import * as Ai from "react-icons/ai";
 import * as Bs from "react-icons/bs";
 import * as Bi from "react-icons/bi";
 import * as Ci from "react-icons/ci";
 import * as Cg from "react-icons/cg";
+import * as Io from "react-icons/io";
 import * as Fa from "react-icons/fa";
 import * as Lu from "react-icons/lu";
 import * as Md from "react-icons/md";
 import * as Gr from "react-icons/gr";
-import * as Hi2 from "react-icons/hi2";
 import * as Si from "react-icons/si";
+import * as Tb from "react-icons/tb";
+import * as Ri from "react-icons/ri";
+import * as Fi from "react-icons/fi";
+import * as Pi from "react-icons/pi";
+import * as Im from "react-icons/im";
+import * as Go from "react-icons/go";
+
 
 
 
@@ -44,9 +54,100 @@ const SidebarIcon = ({ iconName }) => {
             return <Hi2.HiOutlineDocumentChartBar className={className} />;
         case 'GrUserSettings ':
             return <Gr.GrUserSettings className={className} />;
-        case 'SiAmazondocumentdb ':
+        case 'SiAmazondocumentdb':
             return <Si.SiAmazondocumentdb className={className} />;
-
+        case 'GrDocumentNotes':
+            return <Hi2.GrDocumentNotes className={className} />;
+        case 'HiOutlineBanknotes':
+            return <Hi2.HiOutlineBanknotes className={className} />;
+        case 'CiBank':
+            return <Ci.CiBank className={className} />;
+        case 'BsPiggyBank':
+            return <Bs.BsPiggyBank className={className} />;
+        case 'TbCashBanknote':
+            return <Tb.TbCashBanknote className={className} />;
+        case 'MdOutlineCommentBank':
+            return <Md.MdOutlineCommentBank className={className} />;
+        case 'LuBanknote':
+            return <Lu.LuBanknote className={className} />;
+        case 'FaCommentDollar':
+            return <Fa.FaCommentDollar className={className} />;
+        case 'BiCommentCheck':
+            return <Bi.BiCommentCheck ntBank className={className} />;
+        case 'BiCommentDetail':
+            return <Bi.BiCommentDetail className={className} />;
+        case 'BiCommentEdit':
+            return <Bi.BiCommentEdit className={className} />;
+        case 'MdOutlinePayment':
+            return <Md.MdOutlinePayment className={className} />;
+        case 'RiSecurePaymentLine':
+            return <Ri.MdOutlinePaRiSecurePaymentLineyment className={className} />;
+        case 'LiaCommentsDollarSolid':
+            return <Lia.LiaCommentsDollarSolid className={className} />;
+        case 'LiaComment':
+            return <Lia.LiaComment className={className} />;
+        case 'LiaComments':
+            return <Lia.LiaComments className={className} />;
+        case 'FiUsers':
+            return <Fi.FiUsers className={className} />;
+        case 'GrUserSettings':
+            return <Gr.GrUserSettings className={className} />;
+        case 'RiUserStarLine':
+            return <Ri.RiUserStarLine className={className} />;
+        case 'PiMoneyWavyThin':
+            return <Pi.PiMoneyWavyThin className={className} />;
+        case 'CiCircleList':
+            return <Ci.CiCircleList className={className} />;
+        case 'ImMakeGroup':
+            return <Im.ImMakeGroup className={className} />;
+        case 'MdOutlineCoffeeMaker':
+            return <Md.MdOutlineCoffeeMaker className={className} />;
+        case 'LuFolderSync':
+            return <Lu.LuFolderSync className={className} />;
+        case 'BsBackpack':
+            return <Bs.BsBackpack className={className} />;
+        case 'BsBoundingBox':
+            return <Bs.BsBoundingBox className={className} />;
+        case 'AiOutlineDashboard':
+            return <Ai.AiOutlineDashboard className={className} />;
+        case 'IoColorPaletteOutline':
+            return <Io5.IoColorPaletteOutline className={className} />;
+        case 'RiFontColor':
+            return <Ri.RiFontColor className={className} />;
+        case 'AiOutlineUserSwitch':
+            return <Ai.AiOutlineUserSwitch className={className} />;
+        case 'GrDocumentTest':
+            return <Gr.GrDocumentTest className={className} />;
+        case 'GrDocumentCloud':
+            return <Gr.GrDocumentCloud className={className} />;
+        case 'LuUsers2':
+            return <Lu.LuUsers2 className={className} />;
+        case 'TbBrandAirtable':
+            return <Tb.TbBrandAirtable className={className} />;
+        case 'IoIosGitBranch':
+            return <Io.IoIosGitBranch className={className} />;
+        case 'TbFileReport':
+            return <Tb.TbFileReport className={className} />;
+        case 'FaBalanceScale':
+            return <Fa.FaBalanceScale className={className} />;
+        case 'FaUncharted':
+            return <Fa.FaUncharted className={className} />;
+        case 'PiChartLineDownLight':
+            return <Pi.PiChartLineDownLight className={className} />;
+        case 'IoAccessibilityOutline':
+            return <Io5.IoAccessibilityOutline className={className} />;
+        case 'RiUserSettingsLine':
+            return <Ri.RiUserSettingsLine className={className} />;
+        case 'CgChanel':
+            return <Cg.CgChanel className={className} />;
+        case 'MdOutlinePayment':
+            return <Md.MdOutlinePayment className={className} />;
+        case 'GoShieldLock':
+            return <Go.GoShieldLock className={className} />;
+        case 'CgDollar':
+            return <Cg.CgDollar className={className} />;
+        case 'AiOutlineDeliveredProcedure':
+            return <Ai.AiOutlineDeliveredProcedure className={className} />;
 
 
         default:
