9e7b546c55525299d2a36f3d550f99dc9abc2f35
zand23 <52084239+zand23@users.noreply.github.com>
2024-09-30 12:31:29 +0330

    fix ui bachNumber description

--- src/Pages/inventory/batchNumber/description/BatchNumberDescription.jsx
+++ src/Pages/inventory/batchNumber/description/BatchNumberDescription.jsx
@@ -1,14 +1,21 @@
 import React, { useState } from "react";
 import * as api from "@/api";
 import * as url from "@/api/url";
+
 import * as Ant from "antd";
-import { FaBarcode } from "react-icons/fa6";
+// import { FaBarcode } from "react-icons/fa6";
 import useRequestManager from "@/hooks/useRequestManager";
 import ModalHeader from "@/components/common/ModalHeader";
+import CustomContent from "@/components/common/CustomContent";
+
+import { theme } from "antd";
+const { useToken } = theme;
+import { BsCalendar4Week } from "react-icons/bs";
 //====================================================================
 //                        Declaration
 //====================================================================
 const BatchNumberDescription = (props) => {
+  const { token } = useToken();
   const { id } = props;
   const pageTitle = "سری ساخت";
   const [batchNumberData, batchNumberLoading, batchNumberError] = api.useFetch(
@@ -52,22 +59,89 @@ const BatchNumberDescription = (props) => {
   return (
     <>
       <Ant.Skeleton active loading={batchNumberLoading}>
-        <ModalHeader title={pageTitle} />
-        <Ant.Descriptions
-          // column={{
-          //     xs: 1,
-          //     sm: 1,
-          //     md: 4,
-          //     lg: 4,
-          //     xl: 4,
-          //     xxl: 4
-          // }}
-
+        {/* <ModalHeader title={pageTitle} /> */}
+        {/* <Ant.Descriptions
           bordered
           layout="horizontal"
           size="medium"
           items={descriptionItems}
-        />
+        /> */}
+        <ModalHeader title={` مشاهده سری ساخت (${batchNumberData?.data?.batchNumber}) ` } />
+        <Ant.Badge.Ribbon
+          text={`عمر مفید:${batchNumberData?.data?.shelfLife} ماه`}
+        >
+          <CustomContent bordered>
+            <Ant.Row>
+              <Ant.Col xs={24} sm={4} md={4} lg={4}>
+                <Ant.Space direction="vertical" align="center" size="middle">
+                  <Ant.QRCode
+                    size={100}
+                    value={batchNumberData?.data?.batchNumber}
+                    color={token.colorInfoText}
+                  />
+                </Ant.Space>
+              </Ant.Col>
+              <Ant.Col xs={24} sm={18} md={18} lg={18}>
+                <Ant.Row gutter={[8, 16]}>
+                  <Ant.Col span={24}>
+                    <Ant.Typography.Text>
+                      {"سری ساخت"}: {batchNumberData?.data?.batchNumber}
+                    </Ant.Typography.Text>
+                  </Ant.Col>
+                  <Ant.Col xs={24} sm={24} md={12} lg={12}>
+                    {/* <Ant.Row justify="space-around" align="middle">
+                      <Ant.Col span={4}>
+                      {"تاریخ تولید"}:
+                      </Ant.Col>
+
+                      <Ant.Col span={4}>
+                      {"تاریخ تولید"}:dfdfd
+                      </Ant.Col>
+
+                      <Ant.Col span={4}>
+                      {"تاریخ تولید"}:dfdfdf
+                      </Ant.Col>
+
+                      <Ant.Col span={4}>
+                      {"تاریخ تولید"}:dfdf
+                      </Ant.Col>
+
+                    </Ant.Row> */}
+                    <>
+                      <Ant.Space direction="vertical">
+                        <Ant.Typography.Text
+                          type="secondary"
+                          icon={<BsCalendar4Week className="text-blue-600" />}
+                        >
+                          {"تاریخ تولید"}:
+                          {batchNumberData?.data?.productionDate}
+                        </Ant.Typography.Text>
+                        <Ant.Typography.Text type="secondary">
+                          {"تاریخ تولید"}:
+                          {batchNumberData?.data?.gregorianProductionDate}
+                        </Ant.Typography.Text>
+                      </Ant.Space>
+                    </>
+                  </Ant.Col>
+                  <Ant.Col xs={24} sm={24} md={12} lg={12}>
+                    <Ant.Space direction="vertical">
+                      <Ant.Typography.Text
+                        type="secondary"
+                        icon={<BsCalendar4Week className="text-blue-600" />}
+                      >
+                        {"تاریخ انقضا "}:{batchNumberData?.data?.expiryDate}
+                      </Ant.Typography.Text>
+                      <Ant.Typography.Text type="secondary">
+                        {"تاریخ انقضا "}:
+                        {batchNumberData?.data?.gregorianExpiryDate}
+                      </Ant.Typography.Text>
+                    </Ant.Space>
+                  </Ant.Col>
+                </Ant.Row>
+              </Ant.Col>
+            </Ant.Row>
+          </CustomContent>
+        </Ant.Badge.Ribbon>
       </Ant.Skeleton>
     </>
   );
--- src/Pages/salesCommerce/priceCircular/priceCircularDetail/list/PriceCircularDetailList.jsx
+++ src/Pages/salesCommerce/priceCircular/priceCircularDetail/list/PriceCircularDetailList.jsx
@@ -19,150 +19,145 @@ import FormAddPriceCirculardetail from "../add/FormAddPriceCirculardetail";
 //                        Declaration
 //====================================================================
 const PriceCircularDetailList = (props) => {
-  const { priceCircularHeaderId, priceCircularHeaderName } = props;
-  const pageTitle = `جزییات بخشنامه "${priceCircularHeaderName}"`;
-  const [listData, listLoading, listError, listApiCall] =
-    api.useFetchWithHandler();
-  const [delSaving, delLoading, delError, delApiCall] = useDelWithHandler();
-  useRequestManager({ error: delError, loading: delLoading, data: delSaving });
-  const [dataSource, setDataSource] = useState(null);
-  const [modalContent, setModalContent] = useState(null);
-  const [modalState, setModalState] = useState(false);
-  const [modalSize, setModalSize] = useState({ ...defaultValues.MODAL_LARGE });
-  const [pagination, setPagination] = useState({
-    current: 1,
-    pageSize: 10,
-  });
-  useRequestManager({ error: listError });
-
-  //====================================================================
-  //                        useEffects
-  //====================================================================
-  useEffect(() => {
-    getPriceCircularDetailList();
-  }, [priceCircularHeaderId]);
-
-  useEffect(() => {
-    setDataSource(listData?.data);
-  }, [listData]);
-
-  useEffect(() => {
-    delSaving?.isSuccess &&
-      setDataSource([
-        ...dataSource?.filter((c) => c.id !== delSaving?.data?.id),
-      ]);
-  }, [delSaving]);
-
-  //====================================================================
-  //                        Functions
-  //====================================================================
-  const getPriceCircularDetailList = async () => {
-    const queryString = qs.stringify({
-      priceCircularHeaderId: props.priceCircularHeaderId,
+    const { priceCircularHeaderId, priceCircularHeaderName } = props;
+    const pageTitle = `جزییات بخشنامه "${priceCircularHeaderName}"`;
+    const [listData, listLoading, listError, listApiCall] = api.useFetchWithHandler();
+    const [delSaving, delLoading, delError, delApiCall] = useDelWithHandler();
+    useRequestManager({ error: delError, loading: delLoading, data: delSaving });
+    const [dataSource, setDataSource] = useState(null);
+    const [modalContent, setModalContent] = useState(null);
+    const [modalState, setModalState] = useState(false);
+    const [modalSize, setModalSize] = useState({ ...defaultValues.MODAL_LARGE });
+    const [pagination, setPagination] = useState({
+        current: 1,
+        pageSize: 10
     });
-    await listApiCall(`${url.PRICE_CIRCULAR_DETAIL}?${queryString}`);
-  };
+    useRequestManager({ error: listError });
 
-  const onDelete = async (id) => {
-    const req = qs.stringify({
-      id: id,
-    });
-    await delApiCall(`${url.PRICE_CIRCULAR_DETAIL}?${req}`);
-  };
+    //====================================================================
+    //                        useEffects
+    //====================================================================
+    useEffect(() => {
+        getPriceCircularDetailList();
+    }, [priceCircularHeaderId]);
 
-  const onProductView = (productId) => {
-    setModalContent(<DetailProductListDescription id={productId} />);
-    setModalState(true);
-  };
+    useEffect(() => {
+        setDataSource(listData?.data);
+    }, [listData]);
 
-  const onBatchNumberView = (batchNumberId) => {
-    setModalContent(<BatchNumberDescription id={batchNumberId} />);
-    setModalState(true);
-  };
+    useEffect(() => {
+        delSaving?.isSuccess &&
+            setDataSource([
+                ...dataSource?.filter((c) => c.id !== delSaving?.data?.id),
+            ]);
+    }, [delSaving]);
 
-  const handleTableChange = (pagination, filter, sorter) => {
-    setPagination(pagination);
-  };
+    //====================================================================
+    //                        Functions
+    //====================================================================
+    const getPriceCircularDetailList = async () => {
+        const queryString = qs.stringify({
+            priceCircularHeaderId: props.priceCircularHeaderId
+        });
+        await listApiCall(`${url.PRICE_CIRCULAR_DETAIL}?${queryString}`);
+    }
 
-  const onSuccessAdd = () => {
-    setModalState(false);
-    getPriceCircularDetailList();
-  };
+    const onDelete = async (id) => {
+        const req = qs.stringify({
+            id: id
+        })
+        await delApiCall(`${url.PRICE_CIRCULAR_DETAIL}?${req}`);
+    };
 
-  const onAdd = () => {
-    const updateList = { ...defaultValues.MODAL_LARGE, width: 520 };
-    setModalSize(updateList);
-    setModalContent(
-      <FormAddPriceCirculardetail
-        key={uuid.v1()}
-        onSuccess={onSuccessAdd}
-        iD={priceCircularHeaderId}
-      />,
-    );
-    setModalState(true);
-  };
+    const onProductView = (productId) => {
+        setModalContent(<DetailProductListDescription id={productId} />);
+        setModalState(true);
+    }
 
-  const onSuccessEdit = () => {
-    setModalState(false);
-    getPriceCircularDetailList();
-  };
+    const onBatchNumberView = (batchNumberId) => {
+        setModalContent(<BatchNumberDescription id={batchNumberId} />);
+        setModalState(true);
+    }
 
-  const onEdit = (val) => {
-    const updateList = { ...defaultValues.MODAL_LARGE, width: 520 };
-    setModalSize(updateList);
-    setModalContent(
-      <FormEditPriceCircularDetail
-        onSuccess={onSuccessEdit}
-        key={uuid.v1()}
-        id={val.id}
-      />,
-    );
-    setModalState(true);
-  };
+    const handleTableChange = (pagination, filter, sorter) => {
+        setPagination(pagination);
+    }
 
-  //====================================================================
-  //                        Child Components
-  //====================================================================
-  const title = () => {
+    const onSuccessAdd = () => {
+        setModalState(false);
+        getPriceCircularDetailList();
+    };
+
+    const onAdd = () => {
+        const updateList = { ...defaultValues.MODAL_LARGE, width: 520 };
+        setModalSize(updateList);
+        setModalContent(
+            <FormAddPriceCirculardetail key={uuid.v1()} onSuccess={onSuccessAdd} iD={priceCircularHeaderId} />
+        );
+        setModalState(true);
+    };
+
+    const onSuccessEdit = () => {
+        setModalState(false);
+        getPriceCircularDetailList();
+    };
+
+    const onEdit = (val) => {
+        const updateList = { ...defaultValues.MODAL_LARGE, width: 520 };
+        setModalSize(updateList);
+        setModalContent(
+            <FormEditPriceCircularDetail
+                onSuccess={onSuccessEdit}
+                key={uuid.v1()}
+                id={val.id}
+            />
+        );
+        setModalState(true);
+    };
+
+    //====================================================================
+    //                        Child Components
+    //====================================================================
+    const title = () => {
+        return (
+            <ButtonList
+                onAdd={() => {
+                    onAdd();
+                }}
+            />
+        );
+    };
+
+    //====================================================================
+    //                        Component
+    //====================================================================
     return (
-      <ButtonList
-        onAdd={() => {
-          onAdd();
-        }}
-      />
-    );
-  };
-
-  //====================================================================
-  //                        Component
-  //====================================================================
-  return (
-    <>
-      <ModalHeader title={pageTitle} icon={<MdDescription />} />
-      <Ant.Modal
-        open={modalState}
-        {...defaultValues.MODAL_LARGE}
-        {...modalSize}
-        handleCancel={() => setModalState(false)}
-        onCancel={() => {
-          setModalState(false);
-        }}
-        footer={null}
-        centered
-      >
-        {modalContent}
-      </Ant.Modal>
-      <Ant.Table
-        columns={columns(onDelete, onEdit, onProductView, onBatchNumberView)}
-        dataSource={dataSource}
-        pagination={pagination}
-        {...defaultValues.TABLE_PROPS}
-        title={title}
-        onChange={handleTableChange}
-        loading={listLoading}
-      />
-    </>
-  );
-};
+        <>
+            <ModalHeader title={pageTitle} icon={<MdDescription />} />
+            <Ant.Modal
+                open={modalState}
+                {...defaultValues.MODAL_LARGE}
+                {...modalSize}
+                handleCancel={() => setModalState(false)}
+                onCancel={() => {
+                    setModalState(false);
+                }}
+                footer={null}
+                centered
+            >
+                {modalContent}
+            </Ant.Modal>
+            <Ant.Table
+                columns={columns(onDelete, onEdit, onProductView, onBatchNumberView)}
+                dataSource={dataSource}
+                pagination={pagination}
+                {...defaultValues.TABLE_PROPS}
+                title={title}
+                onChange={handleTableChange}
+                loading={listLoading}
+            />
+        </>
+    )
+}
 
 export default PriceCircularDetailList;
