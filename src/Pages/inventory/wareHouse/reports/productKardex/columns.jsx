3a2899c199ae042994f7ec48a23fe6bda1ff2e80
salar taremi <37133468+salarTaremy@users.noreply.github.com>
2024-09-30 08:25:19 +0330

    ok

--- src/Pages/inventory/wareHouse/reports/productKardex/columns.jsx
+++ src/Pages/inventory/wareHouse/reports/productKardex/columns.jsx
@@ -4,71 +4,97 @@ import { RiDeleteBin6Line } from "react-icons/ri";
 import { GrView } from "react-icons/gr";
 import { FiEdit } from "react-icons/fi";
 import * as defaultValues from "@/defaultValues";
-const getDocumentTypeColor = (documentTypeNature) => {
+import { CiLogout, CiLogin } from "react-icons/ci";
+import { MdOutlineFileDownload, MdOutlineFileUpload } from "react-icons/md";
+const getDocumentTypeProp = (documentTypeNature) => {
   switch (documentTypeNature) {
     case 1:
-      return "green";
+      return {color:"green" ,title:"وارده" , className:"text-green-600",operator:" +"};
     case -1:
-      return "red";
+      return {color:"red" ,title:"صادره", className:"text-red-600",operator:" -"};
+      default:
+        return {color:"blue" ,title:"خنثی", className:"",operator:""};
   }
 };
 const columns = () => {
   return [
+
     {
-      title: "شماره برگه",
-      dataIndex: "documentNumber",
-      key: "documentNumber",
-      align: "center",
-      width: 50,
-      className: "text-xs sm:text-sm",
-    },
-    {
-      title: "تاریخ ثبت",
+      title: "تاریخ",
       dataIndex: "issueDate",
       key: "issueDate",
       align: "center",
       width: 80,
       className: "text-xs sm:text-sm",
-      render: (text, record) => `${record.issueDate},${record.issueTime.substr(0,8)}`,
+      render: (text, record) => `${record.issueTime.substr(0, 8)} - ${record.issueDate} `,
     },
     {
-      title: "کد محصول",
-      dataIndex: "productCode",
-      key: "productCode",
-      width: 80,
-      className: "text-xs sm:text-sm",
-    },
-    {
-      title: "نوع برگه",
-      dataIndex: "inventoryDocumentTypeName",
-      key: "inventoryDocumentTypeName",
-      width: 80,
-      className: "text-xs sm:text-sm",
-    },
-    {
-      title: "ماهیت",
-      dataIndex: "inventoryDocumentTypeName",
-      key: "inventoryDocumentTypeName",
-      width: 50,
-      className: "text-xs sm:text-sm",
+      title: "شماره برگه",
+      dataIndex: "documentNumber",
+      key: "documentNumber",
       align: "center",
-      render: (text, record, index) => {
-        return (
-          <Ant.Tag
-            bordered={false}
-            color={getDocumentTypeColor(record?.documentTypeNature)}
-          >
-            {record?.documentTypeNature === 1 ? "IN" : "OUT"}
-          </Ant.Tag>
-        );
-      },
+      width: 30,
+      className: "text-xs sm:text-sm",
     },
+    // {
+    //   title: "کد محصول",
+    //   dataIndex: "productCode",
+    //   key: "productCode",
+    //   width: 80,
+    //   className: "text-xs sm:text-sm",
+    // },
     {
       title: "سری ساخت",
       dataIndex: "batchNumber",
       key: "batchNumber",
-      width: 80,
+      width: 40,
+      align:'center',
       className: "text-xs sm:text-sm",
+    },
+    {
+      title: "شرح",
+      dataIndex: "inventoryDocumentTypeName",
+      key: "inventoryDocumentTypeName",
+      width: 100,
+      className: "text-xs sm:text-sm",
+      render: (text, record, index) => {
+        return (
+          <>
+          {record?.inventoryDocumentTypeName}
+          {' '}
+            {record?.isReserve && <Ant.Tag
+              bordered={false}
+              color={'orange'}
+            >
+              {'رزرو'}
+            </Ant.Tag>}
+          </>)
+      }
+
+
+    },
+    {
+      title: "نوع گردش",
+      dataIndex: "inventoryDocumentTypeName",
+      key: "inventoryDocumentTypeName",
+      width: 30,
+      className: "text-xs sm:text-sm",
+      align: "center",
+      render: (text, record, index) => {
+        return (
+          <>
+
+            {/* {record?.documentTypeNature === 1   ? <MdOutlineFileDownload/> : <MdOutlineFileUpload/>} */}
+            <Ant.Tag
+              bordered={false}
+              color={getDocumentTypeProp(record?.documentTypeNature).color}
+            >
+              {getDocumentTypeProp(record?.documentTypeNature).title}
+            </Ant.Tag>
+
+          </>
+        );
+      },
     },
     {
       title: "تعداد",
@@ -76,6 +102,17 @@ const columns = () => {
       key: "quantity",
       width: 50,
       className: "text-xs sm:text-sm",
+      render: (text, record, index) => {
+        return (
+          <>
+            <Ant.Typography.Text className={getDocumentTypeProp(record?.documentTypeNature).className}>
+
+              {record.quantity}
+              {getDocumentTypeProp(record?.documentTypeNature).operator}
+            </Ant.Typography.Text>
+          </>
+        )
+      }
     },
     {
       title: "مانده",
@@ -83,6 +120,15 @@ const columns = () => {
       key: "balance",
       width: 50,
       className: "text-xs sm:text-sm",
+      render: (text, record, index) => {
+        return (
+          <>
+            <Ant.Typography.Text className='text-blue-600'>
+              {record.balance}
+            </Ant.Typography.Text>
+          </>
+        )
+      }
     },
   ];
 };
