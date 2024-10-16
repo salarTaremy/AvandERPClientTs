5c13d74c89cfb4511b85fed8033d691cb82625f8
mohebali92 <157206069+mohebali92@users.noreply.github.com>
2024-10-16 14:27:38 +0330

    Minor change.

--- src/Pages/inventory/wareHouse/connection/FilterPanel.jsx
+++ src/Pages/inventory/wareHouse/connection/FilterPanel.jsx
@@ -22,7 +22,7 @@ const FilterPanel = (props) => {
     ApiCall,
   ] = useFetchWithHandler();
   const [productTypeData, productTypeLoading, productTypeError] =
-  useFetch(url.PRODUCT_TYPE);
+    useFetch(url.PRODUCT_TYPE);
   const [selectedproductNature, setSelectedProductNature] = useState(null);
   const [form] = Ant.Form.useForm();
   useRequestManager({ error: brandError });
@@ -78,20 +78,20 @@ const FilterPanel = (props) => {
       <Ant.Form form={form} onFinish={onFinish} layout="vertical">
         <Ant.Row gutter={[16, 8]}>
           <Ant.Col md={24} lg={24} sm={24} xs={24}>
-            <Ant.Form.Item name="ProductName" label={"نام محصول"}>
-              <Ant.Input allowClear showCount maxLength={100} />
+            <Ant.Form.Item name="productCode" label={"کد محصول"}>
+              <Ant.Input allowClear showCount maxLength={20} />
             </Ant.Form.Item>
           </Ant.Col>
           <Ant.Col md={24} lg={24} sm={24} xs={24}>
-            <Ant.Form.Item name="productCode" label={"کد محصول"}>
-              <Ant.Input allowClear showCount maxLength={20} />
+            <Ant.Form.Item name="ProductName" label={"نام محصول"}>
+              <Ant.Input allowClear showCount maxLength={100} />
             </Ant.Form.Item>
           </Ant.Col>
           <Ant.Col md={24} lg={24} sm={24} xs={24}>
             <Ant.Form.Item
               name={"productTypeId"}
               label="نوع محصول"
-              // valuePropName="checked"
+            // valuePropName="checked"
             >
               <Ant.Select
                 {...commonOptions}
